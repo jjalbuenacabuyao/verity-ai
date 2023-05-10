c471d7efca42a799fd11dcfc388bebc0c4420f66
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-10 18:30:42 +0800

    remove the functions

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -2,13 +2,13 @@
 
 import React, { useState } from "react";
 import Dropzone, { DropzoneState } from "react-dropzone";
-import { saveAs } from "file-saver";
-import mammoth from "mammoth";
+import {processString, detectText, getTextFromFiles} from "@/utils";
 
 interface FileUploadProps {}
 
 const FileUpload: React.FC<FileUploadProps> = () => {
   const [files, setFiles] = useState<File[]>([]);
+  const [text1, setText1] = useState("")
 
   const handleDrop = (acceptedFiles: File[]) => {
     setFiles(acceptedFiles);
@@ -17,33 +17,12 @@ const FileUpload: React.FC<FileUploadProps> = () => {
   const handleExtract = async () => {
     const text = await getTextFromFiles(files);
     const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "extracted_text.txt");
+    setText1(text);
+    // saveAs(blob, "extracted_text.txt");
   };
 
-  const getTextFromFiles = async (files: File[]) => {
-    let text = "";
-    for (const file of files) {
-      if (file.type === "application/pdf") {
-        // Extract text from PDF using mammoth
-        const result = await mammoth.extractRawText({
-          arrayBuffer: await file.arrayBuffer(),
-        });
-        text += result.value;
-      } else if (
-        file.type ===
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-      ) {
-        // Extract text from DOCX using mammoth
-        const result = await mammoth.extractRawText({
-          arrayBuffer: await file.arrayBuffer(),
-        });
-        text += result.value;
-      } else {
-        console.log("Unsupported file type:", file.type);
-      }
-    }
-    return text;
-  };
+  console.log(processString(text1))
+  // console.log(text1.split("\n"))
 
   return (
     <div>
@@ -60,6 +39,7 @@ const FileUpload: React.FC<FileUploadProps> = () => {
         )}
       </Dropzone>
       <button onClick={handleExtract}>Extract Text</button>
+      <p>{text1}</p>
     </div>
   );
 };
