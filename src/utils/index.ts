abdc3b65f30cecd2cbae3e03ef1d997b3d476898
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-10 18:30:54 +0800

    export modules

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import detectText from "./detectText";
+import getTextFromFiles from "./getTextFromFiles";
+import processString from "./processString";
+
+export {
+  detectText,
+  getTextFromFiles,
+  processString
+}
