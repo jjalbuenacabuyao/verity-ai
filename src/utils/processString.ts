50752bea465b2cad989d48f3c02f5325be04b791
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-22 20:00:42 +0800

    remove the processing (temporarily)

--- src/utils/processString.ts
+++ src/utils/processString.ts
@@ -1,21 +1,9 @@
 export default function processString(str: string) {
-  const splitString = str.split("\n");
-  splitString.map((item, index) => {
-    if (item === "" || item === " " || !(item.trim().endsWith(".") || item.trim().endsWith("?") || item.trim().endsWith("!"))) {
-      delete splitString[index];
-    }
-  })
-  //remove tab in string
-
-  const modifiedString = splitString.join("").trim();
-
-  // if (modifiedString.split(" ").length > 500) {
-  //   for (let i = 0; i < modifiedString.length; i++) {
-  //     if (i >= 400 && (modifiedString[i] === "." || modifiedString[i] === "?" || modifiedString[i] === "!")) {
-  //       return modifiedString.slice(0, i);
-  //     }
-  //   }
+  const splitString = str.trim().split(".");
+  //kukuha ng 10 sentences sa splitString array
+  // for(let i = 0; i <= splitString.length; i++){
+    
   // }
 
-  return modifiedString;
+  return splitString;
 }
