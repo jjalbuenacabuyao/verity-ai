3d5b8c3cab8c5a2e2e0e10816e087f17161b46f1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-23 19:59:38 +0800

    add LogInButton

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Logo from '../Logo/Logo'
-import Nav from '../Nav/Nav'
+import React from "react";
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import LogInButton from "../LogInButton/LogInButton";
+import style from "./header.module.css";
 
-type Props = {}
+type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header>
-      <Logo />
-      <Nav />
+    <header className={`${style.container}`}>
+      <div className={`${style.wrapper}`}>
+        <Logo />
+        <Nav />
+      </div>
+      <LogInButton />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
