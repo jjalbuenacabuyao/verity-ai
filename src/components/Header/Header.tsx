97f693973ab9be424038ecc30a1d016d05a71289
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-26 19:13:50 +0800

    remove wrapper and add menu icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,19 @@
+"use client";
+
 import React from "react";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
-import LogInButton from "../LogInButton/LogInButton";
-import "@styles/utility.css";
 import style from "./header.module.css";
+import { FiMenu } from "react-icons/fi"
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="flex" 
-      data-justify-between 
-      data-items-center
-    >
-      <div className={`${style.wrapper} flex`}>
-        <Logo />
-        <Nav />
-      </div>
-      <LogInButton />
+    <header className={style.header}>
+      <Logo />
+      <Nav />
+      <FiMenu size={24} />
     </header>
   );
 };
