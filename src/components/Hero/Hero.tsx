86d42a1e6b666ddd4973e9f6f50ca42cd31a654a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-26 19:57:59 +0800

    Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import style from "./hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={style.container}>
+      <h1>Detect AI-Generated Text on Student&apos;s Academic Submissions</h1>
+    </div>
+  )
+}
+
+export default Hero
