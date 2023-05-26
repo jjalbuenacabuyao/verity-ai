86565e509c2ea26314f17e8720569f96b5d7c1c5
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-26 19:58:28 +0800

    set as client element

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import Link from 'next/link'
 import React from 'react'
+import style from "./logo.module.css"
 
 type Props = {}
 
 const Logo = (props: Props) => {
   return (
-    <Link href={"/"}>
-      <p>AI Detector</p>
+    <Link href={"/"} className={style.logo}>
+      AI Detector
     </Link>
   )
 }
