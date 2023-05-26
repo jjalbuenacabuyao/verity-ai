f4f334f2da0b7a26ef3c6747caf08dd02cc3cbd4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-26 19:51:40 +0800

    add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
 
 const page = () => {
   return (
-    <Header />
-  )
-}
+    <>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
+};
 
-export default page
+export default page;
