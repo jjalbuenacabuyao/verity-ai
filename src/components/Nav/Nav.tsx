4e95764610b1efeb2248a9c67b96771ca234ba15
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-26 19:14:38 +0800

    add the button

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,20 @@
+"use client";
+
 import Link from 'next/link'
 import style from "./nav.module.css";
+import LogInButton from '../LogInButton/LogInButton';
 
 type Props = {}
 
 const Nav = (props: Props) => {
   return (
-    <nav>
-      <ul className={`${style.list}`}>
+    <nav className={style.nav}>
+      <ul className={style.list}>
         <li>
           <Link href={"/"}>Home</Link>
         </li>
         <li>
-          <Link href={"#features"}>Home</Link>
+          <Link href={"#features"}>Features</Link>
         </li>
         <li>
           <Link href={"#tutorial"}>Tutorial</Link>
@@ -20,6 +23,8 @@ const Nav = (props: Props) => {
           <Link href={"#faq"}>FAQs</Link>
         </li>
       </ul>
+
+      <LogInButton />
     </nav>
   )
 }
