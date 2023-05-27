7d955c438bdc6b691f01c186f542de7903aa2efb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-27 19:43:37 +0800

    add logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,13 +3,15 @@
 import Link from 'next/link'
 import React from 'react'
 import style from "./logo.module.css"
+import Image from 'next/image';
 
 type Props = {}
 
 const Logo = (props: Props) => {
   return (
     <Link href={"/"} className={style.logo}>
-      AI Detector
+      <Image src="/logo.svg" width={36} height={32} alt='Veracity'/>
+      <span>VeracityAI</span>
     </Link>
   )
 }
