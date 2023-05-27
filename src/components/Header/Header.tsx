f254782e57ca82b255036f98bab33995f5eca4d0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-27 13:43:34 +0800

    show border on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import style from "./header.module.css";
@@ -9,8 +9,14 @@ import { FiMenu } from "react-icons/fi"
 type Props = {};
 
 const Header = (props: Props) => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setScrollPosition(window.scrollY));
+  }, [scrollPosition])
+
   return (
-    <header className={style.header}>
+    <header className={`${style.header} ${scrollPosition !== 0 ? style.showBorder : ""}`}>
       <Logo />
       <Nav />
       <FiMenu size={24} fill="#030712" />
