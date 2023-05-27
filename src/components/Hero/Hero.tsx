d28b30e1d6851007e6cd8edef9c8917d7fbaf731
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-05-27 19:43:02 +0800

    add logo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import style from "./hero.module.css";
 
 const Hero = () => {
   return (
     <div className={style.container}>
+      <Image src={"/logo.svg"} width={200} height={200} alt="Veracity" />
       <h1>Detect AI-Generated Text on Student&apos;s Academic Submissions</h1>
     </div>
   )
