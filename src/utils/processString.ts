7783edeec2e82d8ca1ea4d8493469cc3e5ecb555
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-03 19:20:09 +0800

    stable version

--- src/utils/processString.ts
+++ src/utils/processString.ts
@@ -1,27 +1,19 @@
 export default function processString(str: string) {
-  const splitString = str.split("\n");
-  splitString.map((item: string, index: number) => {
-    const trimmedString = item.trim();
-    if(trimmedString === "" 
-       || trimmedString === " " 
-       || !(trimmedString.endsWith(".") || trimmedString.endsWith("?") || trimmedString.endsWith("!"))){
-        delete splitString[index];
-       }
-  });
-  //kukuha ng 10 sentences sa splitString array
-  // for(let i = 0; i <= splitString.length; i++){
-    
-  // }
-  // function is_correct_Sentence(input_str) {
-  //   var first_char = input_str[0];
-  //   var last_char = input_str[input_str.length - 1];
-  //   return /[A-Z]/.test(first_char) && last_char == "."
-  // }
+  const splitString = str.trim().split(".");
+  for (let sentence of splitString) {
+    if (sentence === " " || sentence === "") {
+      delete splitString[splitString.indexOf(sentence)]
+    }
+  }
+  return splitString.map(text => text.trim().replace(/(\r\n|\n|\r)/gm, "").concat("."))
+  // splitString.map((item: string, index: number) => {
+  //   const trimmedString = item.trim();
+  //   if(trimmedString === "" 
+  //      || trimmedString === " " 
+  //      || !(trimmedString.endsWith(".") || trimmedString.endsWith("?") || trimmedString.endsWith("!"))){
+  //       delete splitString[index];
+  //      }
+  // });
 
-  //remove all kinds of line breaks.
-  //someText = someText.replace(/(\r\n|\n|\r)/gm, "");
-  //or
-  // /\r?\n|\r/
-
-  return splitString;
+  // return splitString;
 }
