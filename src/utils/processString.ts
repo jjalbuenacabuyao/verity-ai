6fda644165ad08ef1fe11171e3e57b4c3ee1882f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-05 18:43:49 +0800

    use regex

--- src/utils/processString.ts
+++ src/utils/processString.ts
@@ -1,11 +1,27 @@
+import countWords from "./countWords";
+
 export default function processString(str: string) {
-  const splitString = str.trim().split(".");
-  for (let sentence of splitString) {
-    if (sentence === " " || sentence === "") {
-      delete splitString[splitString.indexOf(sentence)]
-    }
+  const normalizedString = str.trim().replace(/(\r\n|\n|\r)/gm, "");
+  const sentenceArray = normalizedString.split(/[a-z]*[?!.]/g);
+  const sentenceSegments: string[][] = [];
+
+  for (let i = 0; i < sentenceArray.length - 1; i += 10) {
+    const segments = sentenceArray.slice(i, i + 10);
+    sentenceSegments.push(segments);
   }
-  return splitString.map(text => text.trim().replace(/(\r\n|\n|\r)/gm, "").concat("."))
+
+  console.log(sentenceSegments)
+  return sentenceSegments;
+
+  // const splitString = str.trim().split("\n");
+  // for (let sentence of splitString) {
+  //   if (sentence === " " || sentence === "") {
+  //     delete splitString[splitString.indexOf(sentence)]
+  //   }
+  // }
+
+  // console.log(splitString.join("").split(/\s+/))
+  // return splitString.map(text => text.trim().replace(/(\r\n|\n|\r)/gm, "").concat("."))
   // splitString.map((item: string, index: number) => {
   //   const trimmedString = item.trim();
   //   if(trimmedString === "" 
