0ff6db06af29ca8e35f5e4e0fc84d0b21fcf4bbb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-08 21:13:11 +0800

    handle submit

--- src/components/LogInModal/LogInModal.tsx
+++ src/components/LogInModal/LogInModal.tsx
@@ -1,9 +1,33 @@
-import { Root, Trigger, Portal, Overlay, Content, Title } from "@radix-ui/react-dialog";
+import {
+  Root,
+  Trigger,
+  Portal,
+  Overlay,
+  Content,
+  Title,
+} from "@radix-ui/react-dialog";
 import style from "./loginmodal.module.css";
+import LogInButton from "../LogInButton/LogInButton";
+import { FormEvent, useState } from "react";
+import bcrypt from "bcrypt";
 
-type Props = {}
+type Props = {};
 
 const LogInModal = (props: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const formData = {
+      email: email,
+      hashedPassword: hashedPassword
+    }
+  };
+
   return (
     <Root>
       <Trigger asChild>
@@ -13,21 +37,33 @@ const LogInModal = (props: Props) => {
         <Overlay className={style.DialogOverlay} />
         <Content className={style.DialogContent}>
           <Title>Login your account</Title>
-          <form>
+          <form onSubmit={handleSubmit}>
             <fieldset>
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </fieldset>
 
             <fieldset>
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </fieldset>
+
+            <LogInButton />
           </form>
         </Content>
       </Portal>
     </Root>
-  )
-}
+  );
+};
 
-export default LogInModal
+export default LogInModal;
