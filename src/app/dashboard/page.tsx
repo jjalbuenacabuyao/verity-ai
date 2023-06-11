1c9910778bcff6c9c06c65b6e2ebedbc726233c8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-11 19:23:42 +0800

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header/Header";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+};
+
+export default Dashboard;
