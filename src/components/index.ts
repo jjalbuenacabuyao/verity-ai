2d451012167bf60c23b6ab002e7cdc2dc231bd95
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-15 20:05:18 +0800

    initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import AddUserModal from "./AddUserModal/AddUserModal";
+import FileUploadForm from "./FileUploadForm/FileUploadForm";
+import Header from "./Header/Header";
+import LogInButton from "./LogInButton/LogInButton";
+import Hero from "./Hero/Hero";
+import LogInModal from "./LogInModal/LogInModal";
+import Logo from "./Logo/Logo";
+import Nav from "./Nav/Nav";
+
+export {
+  AddUserModal,
+  FileUploadForm,
+  Header,
+  LogInButton,
+  Hero,
+  LogInModal,
+  Logo,
+  Nav,
+}
