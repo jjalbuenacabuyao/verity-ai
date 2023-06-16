07060e96da63fa39dc2afb4e94edbf97cbcdd624
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-16 16:13:39 +0800

    initial commit

---
+++ src/app/aidetector/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from 'react';
+import { Header } from '@/components';
+
+type Props = {}
+
+const AiDetector = (props: Props) => {
+  return (
+    <>
+      <Header />
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default AiDetector
