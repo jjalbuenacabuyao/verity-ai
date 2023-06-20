2ae06026c143b12fb7213cbf7c039714598ffc0a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-20 18:59:19 +0800

    change icon size

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -9,8 +9,8 @@ const Aside = (props: Props) => {
   return (
     <aside className={styles.container}>
       <div className={styles.upload}>
-        <FiUpload size={44} />
-        <h1 className={styles.title}>Upload pdf or docx files</h1>
+        <FiUpload size={36} />
+        <h1 className={styles.title}>Upload pdf and docx files</h1>
         <FileUploadForm />
       </div>
     </aside>
