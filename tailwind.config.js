447b58d27e95baee01e0ac27a6f1a1fd31c1dcd0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-20 20:06:47 +0800

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header": "hsla(210, 20%, 98%, .8)",
+        "off-black": "hsla(240, 6%, 25%, 0.122)",
+      }
+    },
   },
   plugins: [],
 }
