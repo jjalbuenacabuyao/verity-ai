d8870e4e491e906a125f7730a818758132036aab
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-23 20:15:05 +0800

    initial commit

---
+++ src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { TailSpin } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <div className="grid gap-3 text-center">
+      <TailSpin
+        height="40"
+        width="40"
+        color="#0ea5e9"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass="justify-center"
+        visible={true}
+      />
+      <span>Loading</span>
+    </div>
+  );
+};
+
+export default Loader;
