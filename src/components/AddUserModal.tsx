69d5e45325872baa7c6c748553bd3c2142c771a4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-24 19:13:48 +0800

    remove use client

--- src/components/AddUserModal.tsx
+++ src/components/AddUserModal.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import {
   Root,
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,17 @@
-"use client";
-
 import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="py-24 px-6 flex flex-col items-center text-center">
+    <div className="flex flex-col items-center px-6 py-24 text-center">
       <Image src={"/logo.svg"} width={200} height={200} alt="Veracity" />
       <div className="grid gap-4">
-        <h1 className="bg-gradient-to-tr from-light-violet to-light-blue text-transparent bg-clip-text font-work-sans font-black text-2xl">VerityAI</h1>
+        <h1 className="bg-gradient-to-tr from-light-violet to-light-blue bg-clip-text font-work-sans text-2xl font-black text-transparent">
+          VerityAI
+        </h1>
         <p>Detect AI-Generated Academic Submissions</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
