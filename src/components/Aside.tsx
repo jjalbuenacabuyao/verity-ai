9d5371baf43326365685845fc7376f21dd5c9f4d
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-24 19:12:44 +0800

    add props

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -2,18 +2,13 @@ import React, { Dispatch, SetStateAction } from "react";
 import FileUploadForm from "./FileUploadForm";
 import { FiUpload } from "react-icons/fi";
 
-interface Props {
-  setFiles: Dispatch<SetStateAction<File[]>>;
-  setIsLoading: Dispatch<SetStateAction<boolean>>;
-}
-
-const Aside = ({ setFiles, setIsLoading }: Props) => {
+const Aside = () => {
   return (
     <aside className="mt-20 mb-16">
       <div className="flex flex-col items-center rounded-lg border-2 border-dashed py-6">
         <FiUpload size={36} />
         <h1 className="mb-6 mt-4">Upload pdf and docx files</h1>
-        <FileUploadForm setFiles={setFiles} setIsLoading={setIsLoading} />
+        <FileUploadForm />
       </div>
     </aside>
   );
