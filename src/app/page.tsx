a92bf7e42953c2f3cb031314ab1654badeafa1d3
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-24 19:11:18 +0800

    add state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
 import { Header, Hero } from "@/components";
+import { useEffect, useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header
+        scrollPosition={scrollPosition}
+        navOpen={navOpen}
+        setNavOpen={setNavOpen}
+      />
       <main>
         <Hero />
       </main>
@@ -13,4 +33,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
