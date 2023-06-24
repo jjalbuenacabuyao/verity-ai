b772111e332ca7933ccb6ea80062779083610cf4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-24 19:11:37 +0800

    add state

--- src/app/aidetector/page.tsx
+++ src/app/aidetector/page.tsx
@@ -1,22 +1,40 @@
 "use client";
 
-import React, { useState } from 'react';
-import { Aside, Header, Result } from '@/components';
-
+import React, { useEffect, useState } from "react";
+import { Header } from "@/components";
+import FileContext from "@/hooks/FileContext";
+import AiDetectorMain from "@/components/AiDetectorMain";
 
 const AiDetector = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      <Header />
-      <main className="mx-6">
-        <Aside setFiles={setFiles} setIsLoading={setIsLoading} />
-        <Result setIsLoading={setIsLoading} files={files} isLoading={isLoading} />
-      </main>
+      <Header
+        scrollPosition={scrollPosition}
+        navOpen={navOpen}
+        setNavOpen={setNavOpen}
+      />
+      <FileContext.Provider value={{files, setFiles, isLoading, setIsLoading}}>
+        <AiDetectorMain />
+      </FileContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default AiDetector
+export default AiDetector;
