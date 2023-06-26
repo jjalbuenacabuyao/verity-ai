dc556819d7cb7669852591b1b465fe5fec561ed2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-26 22:24:07 +0800

    only accepts one file at a time

--- src/utils/getTextFromFiles.ts
+++ src/utils/getTextFromFiles.ts
@@ -6,41 +6,41 @@ import mammoth from "mammoth";
  * @param {File[]} files - an array of File objects, which represent files selected by the user through a file input field. These files can be of type PDF or DOCX. The function extracts text from these files.
  * @returns an array of objects, where each object contains the filename and the extracted text from
  * the corresponding file.
-*/
+ */
 
 interface FileInfo {
   filename: string;
   extractedText: string;
 }
 
-export default async function getTextFromFiles(files: File[]) {
+export default async function getTextFromFiles(file: File) {
   const pdfFileType = "application/pdf";
-  const docxFileType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+  const docxFileType =
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 
-  const extractedText: FileInfo[] = []
+  let extractionInfo: FileInfo = {filename: file.name, extractedText: ""};
+  let text = "";
+
+  if (file.type === pdfFileType) {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await fetch("/api/pdfHandler", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    text += data.text;
+    extractionInfo.extractedText = text;
+  }
   
-  for (const file of files) {
-    let text = "";
-
-    if (file.type === pdfFileType) {
-      const formData = new FormData();
-      formData.append('file', file);
-      const response = await fetch('/api/fileHandler', {
-        method: 'POST',
-        body: formData,
-      });
-      const data = await response.json();
-      text += data.text;
-      extractedText.push({filename: file.name, extractedText: text})
-    } else if (file.type === docxFileType) {
-      // Extract text from DOCX using mammoth library.
-      const result = await mammoth.extractRawText({
-        arrayBuffer: await file.arrayBuffer(),
-      });
-      text += result.value;
-      extractedText.push({filename: file.name, extractedText: text})
-    }
+  else if (file.type === docxFileType) {
+    // Extract text from DOCX using mammoth library.
+    const result = await mammoth.extractRawText({
+      arrayBuffer: await file.arrayBuffer(),
+    });
+    text += result.value;
+    extractionInfo.extractedText = text;
   }
 
-  return extractedText;
-};
+  return extractionInfo;
+}
