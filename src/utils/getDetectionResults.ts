de373af7a5b337b361dd527d2f3bb28b565e7eda
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-26 22:23:52 +0800

    refactoring dependency functions

--- src/utils/getDetectionResults.ts
+++ src/utils/getDetectionResults.ts
@@ -9,23 +9,34 @@ export default async function getDetectionResult(files: File[]) {
 
   const extractedText = textFromFiles.map((item) => item.extractedText);
 
-  const normalizedText = extractedText.map(text => cleanText(text))
+  const normalizedText = extractedText.map((text) => cleanText(text));
 
-  const sentences = normalizedText.map(text => filterSentences(text).join(" "));
+  const sentences = normalizedText.map((text) =>
+    filterSentences(text).join(" ")
+  );
 
-  const allText = sentences.map(sentence => sentence);
+  const allText = sentences.map((sentence) => sentence);
 
   const result = allText.map(async (item) => {
     const wordCount = countWords(item);
     if (wordCount === 0) {
       return "The document does not contain text.";
     }
-  
+
     if (wordCount <= 300) {
-      const data = await detectText(item);
-      return data;
+      // const JSONdata = JSON.stringify(item);
+
+      // const endpoint = "/api/detectionHandler";
+
+      // const options = {
+      //   method: "POST",
+      //   body: JSONdata,
+      // };
+
+      // const response = await fetch(endpoint, options);
+      // return await response.json();
     }
-  })
+  });
 
   return result;
 }
