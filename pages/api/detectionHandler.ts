b6c91e01164352ec8802c1672fe8fa8fd086c6c2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-27 18:56:56 +0800

    add types

--- pages/api/detectionHandler.ts
+++ pages/api/detectionHandler.ts
@@ -2,6 +2,7 @@ import { cleanText, countWords, detectText, filterSentences, getTextFromFiles } 
 import formidable from "formidable";
 import { NextApiRequest, NextApiResponse } from "next";
 
+
 export const config = {
   api: {
     bodyParser: false,
@@ -14,22 +15,27 @@ export default async function detectionHandler(
 ) {
   const form = new formidable.IncomingForm();
   form.parse(request, async (err, fields, files) => {
-    const file = files.file as unknown as File;
+    const file: formidable.File = files.file as formidable.File;
 
-    const { filename, extractedText } = await getTextFromFiles(file);
+    const { originalFilename } = file;
+
+    const { extractedText } = await getTextFromFiles(file as unknown as File);
     
     const normalizedText = cleanText(extractedText);
 
     const wordCount = countWords(normalizedText);
     //initial pa lang
-    if (wordCount <= 250) {
-      const detectionResult = await detectText(normalizedText);
-      const { label, score } = detectionResult;
-      const humanWritten = "LABEL_0";
-      const aiGenerated = "LABEL_1";
-      if (label === humanWritten && !(Math.trunc(score * 100) >= 5)) {
-        response.status(200).json({ filename: filename, label: label, score: score })
-      }
-    }
+    // if (wordCount <= 250) {
+    //   const detectionResult = await detectText(normalizedText);
+    //   const { label, score } = detectionResult;
+    //   const humanWritten = "LABEL_0";
+    //   const aiGenerated = "LABEL_1";
+    //   if (label === humanWritten && !(Math.trunc(score * 100) >= 5)) {
+    //     response.status(200).json({ filename: filename, label: label, score: score })
+    //   }
+    // }
+
+    const result = await detectText(normalizedText);
+    response.status(200).json([originalFilename, result])
   })
 }
