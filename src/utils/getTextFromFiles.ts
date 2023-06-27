f9418cd09c55d48c002ddc36f0bd643816261c7c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-27 20:12:38 +0800

    return string

--- src/utils/getTextFromFiles.ts
+++ src/utils/getTextFromFiles.ts
@@ -1,25 +1,11 @@
 import mammoth from "mammoth";
 
-/**
- * This function extracts text from PDF and DOCX files and returns an array of objects containing the
- * filename and extracted text.
- * @param {File[]} files - an array of File objects, which represent files selected by the user through a file input field. These files can be of type PDF or DOCX. The function extracts text from these files.
- * @returns an array of objects, where each object contains the filename and the extracted text from
- * the corresponding file.
- */
-
-interface FileInfo {
-  filename: string;
-  extractedText: string;
-}
-
 export default async function getTextFromFiles(file: File) {
   const pdfFileType = "application/pdf";
   const docxFileType =
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
 
-  let extractionInfo: FileInfo = {filename: file.name, extractedText: ""};
-  let text = "";
+  let extractedTexts: string = "";
 
   if (file.type === pdfFileType) {
     const formData = new FormData();
@@ -29,8 +15,7 @@ export default async function getTextFromFiles(file: File) {
       body: formData,
     });
     const data = await response.json();
-    text += data.text;
-    extractionInfo.extractedText = text;
+    extractedTexts = data.text;
   }
   
   else if (file.type === docxFileType) {
@@ -38,9 +23,8 @@ export default async function getTextFromFiles(file: File) {
     const result = await mammoth.extractRawText({
       arrayBuffer: await file.arrayBuffer(),
     });
-    text += result.value;
-    extractionInfo.extractedText = text;
+    extractedTexts = result.value;
   }
 
-  return extractionInfo;
+  return extractedTexts;
 }
