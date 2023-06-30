7abafaf8bf7de3eb7a3686b23f26810f1815d708
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-06-30 20:55:45 +0800

    regex for removing multiple space and new line

--- src/utils/cleanText.ts
+++ src/utils/cleanText.ts
@@ -1,7 +1,9 @@
 export default function cleanText(text: string): string {
   const trimmedText: string = text.trim();
   const normalizedString: string = trimmedText
-    .replace(/(\r\n|\n|\r)/gm, " ")
-    .replace(/[ ]{2,}/gi, " ");
+    // .replace(/(\r\n|\n|\r)/gm, " ")
+    // .replace(/\n+/g, '\n')
+    .replace(/ +/g, ' ');
+    // .replace(/[ ]{2,}/gi, " ");
   return normalizedString;
 }
