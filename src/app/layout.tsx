f915219bf973b71f2a0b4221d87f9666076d579b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-08 20:17:14 +0800

    edit title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "VerityAI | Detect AI-Generated Academic Submissions",
+  title: "VerityAI",
   description: "Detect AI-Generated Academic Submissions",
 };
 
