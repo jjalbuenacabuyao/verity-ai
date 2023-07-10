6a08d54b96356408b68c9bbaa3a50afb268831d2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-10 09:38:32 +0800

    modify the color based on percentage

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,26 +1,35 @@
 import React from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 interface Props {
   filename: string;
-  aiGeneratedPercentage: number | string;
+  aiGeneratedPercentage: string;
 }
 
 const Result = ({ filename, aiGeneratedPercentage }: Props) => {
+  const value = parseFloat(aiGeneratedPercentage);
+  const pathColor = `${value >= 90
+      ? "#ef4444"
+      : value < 90 && value > 10
+        ? "#f97316"
+        : "#0ea5e9"
+    }`;
+  const textColor = pathColor;
+  
   return (
-    <div className="flex items-center justify-between">
-      <span>{filename}</span>
+    <div className="grid grid-cols-[75%_25%] items-center p-3 border rounded-md shadow-sm">
+      <span className="text-sm">{filename}</span>
       <div className="h-auto w-12">
         <CircularProgressbar
           minValue={0}
           maxValue={100}
           text={`${aiGeneratedPercentage}%`}
-          value={
-            typeof aiGeneratedPercentage === "string"
-              ? parseFloat(aiGeneratedPercentage)
-              : aiGeneratedPercentage
-          }
+          value={value}
+          styles={buildStyles({
+            pathColor: pathColor,
+            textColor: textColor
+          })}
         />
       </div>
     </div>
