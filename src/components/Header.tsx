d15f577f0333ef0cbae273f0f42e5814eaeea13a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-10 09:37:22 +0800

    fix the color of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,16 +7,17 @@ import { AiOutlineClose } from "react-icons/ai";
 interface Props {
   scrollPosition: number;
   navOpen: boolean;
-  setNavOpen: Dispatch<SetStateAction<boolean>>
+  setNavOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 const Header = ({ scrollPosition, navOpen, setNavOpen }: Props) => {
   return (
     <header
-      className={`fixed inset-x-0 top-0 z-10 flex items-center justify-between border-b-[1px] border-solid border-transparent bg-header px-6 py-4 backdrop-blur backdrop-saturate-50 transition-colors duration-75 ease-linear ${
-        scrollPosition !== 0 ? "border-b-off-black" : ""
-      }`}
-    >
+      className={`fixed inset-x-0 top-0 z-10 flex items-center justify-between border-b-[1px] border-solid border-transparent px-6 py-4 transition-colors duration-75 ease-linear ${
+        scrollPosition !== 0
+          ? "border-b-off-black bg-header backdrop-blur backdrop-saturate-50"
+          : ""
+      }`}>
       <Logo />
       <Nav navOpen={navOpen} />
 
