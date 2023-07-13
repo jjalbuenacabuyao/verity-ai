c95668a2e6434cffd0db0e6094a87e6a989230ea
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 10:58:23 +0800

    remove the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,9 @@
-"use client";
-
-import { Header, Hero } from "@/components";
-import { useEffect, useState } from "react";
+import { Hero } from "@/components";
 
 const Page = () => {
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const [navOpen, setNavOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <>
-      <Header
-        scrollPosition={scrollPosition}
-        navOpen={navOpen}
-        setNavOpen={setNavOpen}
-      />
-      <main>
-        <Hero />
-      </main>
+      <Hero />
     </>
   );
 };
