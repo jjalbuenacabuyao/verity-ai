50c070aaeffd0250e0ae23b4a0add3e9e82e2389
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 10:59:28 +0800

    wrap in div instead of main

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,16 @@
 "use client";
 
 import { Header, AddUserModal } from "@/components";
-import { useEffect, useState } from "react";
-import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <>
-      <main>
-        <div>
-          <h1>Users</h1>
-          <button>+ Add New User</button>
-        </div>
-        <AddUserModal />
-      </main>
-    </>
+    <div>
+      <div>
+        <h1>Users</h1>
+        <button>+ Add New User</button>
+      </div>
+      <AddUserModal />
+    </div>
   );
 };
 
