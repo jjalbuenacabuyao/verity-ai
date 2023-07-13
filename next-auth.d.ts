094d2a4075c456f93c87f49430a0bfabeeef6cf1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 09:13:52 +0800

    add name, id and hashed password

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,16 +1,19 @@
-import 'next-auth'
+import "next-auth";
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface User {
-    role: string
+    name: string;
+    id: string;
+    hashedPassword: string;
+    role: string;
   }
 
   interface Session {
     user: {
-      name?: string | null
-      email?: string | null
-      image?: string | null
-      role: string
-    }
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string | null;
+    };
   }
 }
