9af379343f491c4cbb2b342d31ab86521e448e83
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 10:59:45 +0800

    remove header

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -1,40 +1,17 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Aside, Header, ResultContainer } from "@/components";
+import { Aside, ResultContainer } from "@/components";
 
-const AiDetector = () => {
+const Detector = () => {
   const [files, setFiles] = useState<File[]>([]);
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const [navOpen, setNavOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
-    <>
-      <Header
-        scrollPosition={scrollPosition}
-        navOpen={navOpen}
-        setNavOpen={setNavOpen}
-      />
-      <main className="mx-6">
-        <Aside setFiles={setFiles} />
-        <ResultContainer
-          files={files}
-        />
-      </main>
-    </>
+    <div className="mx-6">
+      <Aside setFiles={setFiles} />
+      <ResultContainer files={files} />
+    </div>
   );
 };
 
-export default AiDetector;
+export default Detector;
