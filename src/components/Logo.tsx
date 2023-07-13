151a055ddd39223794454489b0586b4919315324
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 11:01:50 +0800

    add font-bold classname

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = (props: Props) => {
   return (
     <Link href={"/"} className="z-[9999] flex items-center font-work-sans">
       <Image src="/logo.svg" width={36} height={32} alt="Veracity" />
-      <span>VerityAI</span>
+      <span className="font-bold">VerityAI</span>
     </Link>
   );
 };
