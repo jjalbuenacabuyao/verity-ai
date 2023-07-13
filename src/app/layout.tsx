e3972a3d299dee7f388684b76173a4c0230c3e7b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-13 10:58:03 +0800

    add currentUser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
+import { Header, Provider } from "@/components";
 import "./globals.css";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: "VerityAI",
   description: "Detect AI-Generated Academic Submissions",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+  }) {
+  
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
-      <body className="font-inter text-slate-900">{children}</body>
+      <body className="font-inter text-slate-900">
+        <Provider>
+          <Header />
+          <main>{children}</main>
+        </Provider>
+      </body>
     </html>
   );
 }
