a63efdaed8e4c728135ec96081c4e73db2ffe966
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-14 08:33:58 +0800

    export CurrentUserProvider

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,25 +1,25 @@
 import AddUserModal from "./AddUserModal";
 import Aside from "./Aside";
+import CurrentUserProvider from "./CurrentUserProvider";
 import Header from "./Header";
 import Hero from "./Hero";
 import LogInButton from "./LogInButton";
 import LogInModal from "./LogInModal";
 import Logo from "./Logo";
 import Nav from "./Nav";
-import Provider from "./Provider";
 import Result from "./Result";
 import ResultContainer from "./ResultContainer";
 
 export {
   AddUserModal,
   Aside,
+  CurrentUserProvider,
   Header,
   Hero,
   LogInButton,
   LogInModal,
   Logo,
   Nav,
-  Provider,
   Result,
   ResultContainer,
 };
