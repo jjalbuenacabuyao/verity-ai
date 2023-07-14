a2090a6e1443d5ffcfc25ac43d0380e17689c546
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-14 10:12:19 +0800

    use Button component and seperate the LoginModal

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
+import { Root, Trigger } from "@radix-ui/react-dialog";
+import LogInModal from "./LogInModal";
+import Button from "./Button";
 
-type Props = {};
-
-const LogInButton = (props: Props) => {
-  return <button type="submit">Login</button>;
+const LogInButton = () => {
+  return (
+    <Root>
+      <Trigger asChild>
+        <Button variant="primary" text="Login" />
+      </Trigger>
+      <LogInModal />
+    </Root>
+  )
 };
 
 export default LogInButton;
