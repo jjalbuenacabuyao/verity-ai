5ff6b16824f2fbbf068e53f394fc28f08eabe16a
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-16 18:54:18 +0800

    declare fonts from next/font/google

---
+++ src/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Inter, Work_Sans } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap"
+});
+
+export const workSans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap"
+});
