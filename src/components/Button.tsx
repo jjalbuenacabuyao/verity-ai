f7b15d7db7438fd319bef505b98a435fe7c8d80f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-16 18:54:53 +0800

    format code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 interface Props {
   variant: "primary" | "secondary" | "toggler";
@@ -13,12 +13,11 @@ const Button = ({ variant, type, text, onClick, children }: Props) => {
     <button
       type={type}
       onClick={onClick}
-      className={`${variant} px-6 py-2 text-sm rounded-full`}
-    >
+      className={`${variant} rounded-full px-6 py-2 text-sm`}>
       {text}
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
