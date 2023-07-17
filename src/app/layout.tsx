eb9d4e3db584feacf316248ea5e4800e1a366b46
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-17 15:32:09 +0800

    change the font color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${inter.className} text-slate-900`}>
+      <body className={`${inter.className} text-slate-600`}>
         <CurrentUserProvider currentUser={currentUser}>
           <Header />
           <main>{children}</main>
