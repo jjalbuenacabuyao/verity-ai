fd1614e7f760d716c79cc31547f50fb11e1eb9bd
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-17 20:50:52 +0800

    increase fontsize

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
   return (
     <Link href={"/"} className="z-[9999] flex items-center">
       <Image src="/logo.svg" width={36} height={32} alt="Veracity" />
-      <span className={`${workSans.className}`}>VerityAI</span>
+      <span className={`${workSans.className} text-lg`}>VerityAI</span>
     </Link>
   );
 };
