f052118a1e96208a03e29b3efb37573413823e00
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-17 20:50:25 +0800

    add logo and use InputField component

--- src/components/LogInModal.tsx
+++ src/components/LogInModal.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import {
-  Portal,
-  Overlay,
-  Content,
-  Title,
-} from "@radix-ui/react-dialog";
+import { Portal, Overlay, Content, Title } from "@radix-ui/react-dialog";
 import { FormEvent } from "react";
 import { signIn } from "next-auth/react";
 import Button from "./Button";
+import InputField from "./InputField";
+import { workSans } from "@/fonts";
+import Image from "next/image";
 
 const LogInModal = () => {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -26,20 +24,31 @@ const LogInModal = () => {
   return (
     <Portal>
       <Overlay className="fixed inset-0 z-20 animate-overlayShow bg-black/[0.44]" />
-      <Content className="fixed left-1/2 top-1/2 z-20 max-h-[85vh] w-[90vw] max-w-md -translate-x-1/2 -translate-y-1/2 animate-contentShow rounded-md bg-white p-6 shadow-dialog-content">
-        <Title>Login your account</Title>
+      <Content className="fixed left-1/2 top-1/2 z-20 max-h-[85vh] w-[90vw] max-w-md -translate-x-1/2 -translate-y-1/2 animate-contentShow rounded-md bg-white p-8 shadow-dialog-content overflow-auto">
+        <Image src="/logo.svg" width={40} height={44} alt="VerityAI" className="mx-auto mb-2" />
+        <Title className={`${workSans.className} mb-12 text-center text-lg`}>
+          Log in your account
+        </Title>
         <form onSubmit={handleSubmit}>
-          <fieldset>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required />
-          </fieldset>
+          <fieldset className="text-right">
+            <InputField
+              type="email"
+              id="email"
+              name="email"
+              required
+              label="Email Address"
+            />
 
-          <fieldset>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required />
-          </fieldset>
+            <InputField
+              type="password"
+              id="password"
+              name="password"
+              required
+              label="Password"
+            />
 
-          <Button text="Log in" variant="primary" type="submit" />
+            <Button text="Log in" variant="primary" type="submit" />
+          </fieldset>
         </form>
       </Content>
     </Portal>
