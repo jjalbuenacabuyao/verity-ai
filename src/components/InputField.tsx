e43f5ff5422ea43d186566bcf5c316b67c00f3bd
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-17 20:48:45 +0800

    initial commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+import { BiHide, BiShowAlt } from "react-icons/bi";
+import Button from "./Button";
+
+interface Props {
+  type: React.HTMLInputTypeAttribute;
+  id: string;
+  name: string;
+  required: boolean;
+  label: string;
+}
+
+const InputField = ({ type, id, name, required, label }: Props) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <div className="relative mb-6">
+      <input
+        className="peer h-12 w-full rounded-md border border-gray-300 pl-3 placeholder-transparent autofill:bg-transparent focus:outline-sky-500"
+        type={type === "password" && showPassword ? "text" : type}
+        id={id}
+        name={name}
+        required={required}
+        placeholder={label}
+        autoComplete="off"
+      />
+      <label
+        htmlFor={id}
+        className="absolute -top-2.5 left-3 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-2.5  peer-focus:text-sm peer-focus:text-gray-600">
+        {label}
+      </label>
+
+      {type === "password" && (
+        <Button
+          type="button"
+          variant="toggler"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute top-3.5 right-3">
+          {showPassword ? <BiShowAlt size={20} /> : <BiHide size={20} />}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
