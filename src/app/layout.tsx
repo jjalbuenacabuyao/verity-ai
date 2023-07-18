b7efb2712b6cd4e215e72625f9c516b9ca300012
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-18 21:39:55 +0800

    change font color and add antialiased class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${inter.className} text-slate-600`}>
+      <body className={`${inter.className} text-slate-700 antialiased`}>
         <CurrentUserProvider currentUser={currentUser}>
           <Header />
           <main>{children}</main>
