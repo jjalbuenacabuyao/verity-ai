de125ea8f3bf7e4f5fb6e2e772518b0f372d041d
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-19 20:22:55 +0800

    add ToastProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { Header, CurrentUserProvider } from "@/components";
+import { Header } from "@/components";
+import { CurrentUserProvider, ToastProvider } from "@/providers";
 import getCurrentUser from "./actions/getCurrentUser";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -11,23 +12,24 @@ export const metadata = {
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-  weight: ["400", "500", "600", "700"]
+  weight: ["400", "500", "600", "700"],
 });
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-  }) {
-  
+}) {
   const currentUser = await getCurrentUser();
 
   return (
     <html lang="en">
       <body className={`${inter.className} text-slate-700 antialiased`}>
         <CurrentUserProvider currentUser={currentUser}>
-          <Header />
-          <main>{children}</main>
+          <ToastProvider>
+            <Header />
+            <main>{children}</main>
+          </ToastProvider>
         </CurrentUserProvider>
       </body>
     </html>
