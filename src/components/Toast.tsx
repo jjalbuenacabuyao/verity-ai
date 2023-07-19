e35c53e0536b3718288c3aaabb1be24195fe7a19
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-19 20:24:46 +0800

    initial commit

---
+++ src/components/Toast.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { Dispatch, SetStateAction } from "react";
+import {
+  Root,
+  Title,
+  Description,
+  Close,
+  Viewport,
+} from "@radix-ui/react-toast";
+import { BiErrorCircle } from "react-icons/bi";
+import { AiFillCloseCircle } from "react-icons/ai"
+
+type Props = {
+  filename: string;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const Toast: React.FC<Props> = ({ isOpen, setIsOpen, filename }: Props) => {
+  return (
+    <>
+      <Root
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="grid grid-cols-[max-content_auto_max-content] grid-rows-2 items-center rounded-md bg-red-500 p-4 text-white shadow-md gap-x-6 gap-y-2 data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut">
+        
+        <BiErrorCircle size={32} className="row-span-2" />
+
+        <Title className="leading-none text-sm">Error occured while processing:</Title>
+
+        <Description className="col-start-2 row-start-2 font-medium leading-none">
+          {filename}
+        </Description>
+
+        <Close className="col-start-3 row-span-2 row-start-1 ml-4">
+          <AiFillCloseCircle size={24} />
+        </Close>
+      </Root>
+      <Viewport className="fixed bottom-0 right-0 z-[31416] flex max-w-full flex-col gap-3 p-6" />
+    </>
+  );
+};
+
+export default Toast;
