eb84198a7b1cdff0ed967995bf019231190df4c2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-20 19:08:30 +0800

    add shadow on hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,14 +9,21 @@ interface Props {
   className?: string;
 }
 
-const Button = ({ variant, type, text, onClick, children, className }: Props) => {
+const Button = ({
+  variant,
+  type = "button",
+  text,
+  onClick,
+  children,
+  className = "",
+}: Props) => {
   return (
     <button
       type={type}
       onClick={onClick}
       className={`${variant} ${
         variant !== "toggler"
-          ? "rounded-full px-6 py-2 font-medium tracking-wide"
+          ? "rounded-full px-6 py-2 font-medium tracking-wide transition-shadow hover:shadow-lg hover:shadow-gray-400"
           : "z-10"
       } ${className}`}>
       {text}
