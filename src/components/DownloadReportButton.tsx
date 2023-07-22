dde559378dbc09000b072f2e7e9b4e76e78b1d79
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-22 23:25:48 +0800

    use the createDocumentMarkup function

--- src/components/DownloadReportButton.tsx
+++ src/components/DownloadReportButton.tsx
@@ -1,56 +1,44 @@
+import { ResultWithFilename } from "@/types";
+import { createDocumentMarkup } from "@/utils";
 import { saveAs } from "file-saver";
 import { asBlob } from "html-docx-ts";
 
-const HtmlStr = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-  <meta http-equiv="Pragma" content="no-cache" />
-  <meta http-equiv="Expires" content="0" />
-  <title>testTitle</title>
-<body>
-    <div style="color:red;font-family:Arial;font-weight:bold;">
-        testContext
-    </div >
-    <table>
-  <tr>
-    <th>Month</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <td>January</td>
-    <td>$100</td>
-  </tr>
-  <tr>
-    <td>February</td>
-    <td>$80</td>
-  </tr>
-</table>
-</body >
-</html >`;
+interface Props {
+  results: ResultWithFilename[];
+}
 
-const DownloadReportButton = () => {
+const DownloadReportButton = ({ results }: Props) => {
+  const html = createDocumentMarkup(results);
+
   const date = new Date();
 
   const year = date.getFullYear();
   const month = date.toLocaleString("default", { month: "short" });
   const dateToday = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-  const option = { orientation: "portrait", margins: {}};
+  const dateString = `${year}-${month}-${dateToday}`;
+
+  const option = { orientation: "landscape", margins: {} };
 
   const headerConfig = {
-    leftStr: "VerityAI",
-    rightStr: `${year} ${month} ${dateToday}`,
+    leftStr: "VerityAI Detection Report",
+    rightStr: dateString,
   };
 
   const footerConfig = {
     rightStr: "VerityAI",
   };
 
-  async function downloadDocx() { //@ts-ignore
-    asBlob(HtmlStr, option, headerConfig, footerConfig).then((blobData) => {
-      saveAs(blobData as Blob, `Detection Result.docx`); // save as docx document
+  async function downloadDocx() {
+    //@ts-ignore
+    asBlob(html, option, headerConfig, footerConfig).then((blobData) => {
+      saveAs(
+        blobData as Blob,
+        `Detection Result (${dateString}) ${hour}${minute}${second}.docx`
+      ); // save as docx document
     });
   }
   return <button onClick={downloadDocx}>Download Report</button>;
