aa9330241c4c71a873b331e54a9f0b80b07a3691
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-26 12:17:51 +0800

    import CircularProgress

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -8,8 +8,8 @@ import {
 } from "@radix-ui/react-accordion";
 import { ResultWithFilename } from "@/types";
 import { FaChevronDown } from "react-icons/fa";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import CircularProgress from "./CircularProgress";
 
 interface Props {
   data: ResultWithFilename[];
@@ -21,40 +21,19 @@ const Accordion = ({ data }: Props) => {
       type="single"
       collapsible
       className="scrollbar grid gap-y-4 overflow-y-auto lg:max-h-[65%] lg:pr-2">
-      {data.map((item, index) => {
-        const value = parseFloat(item.result.aiGeneratedPercentage);
-        const pathColor = `${
-          value >= 90
-            ? "#ef4444"
-            : value < 90 && value > 10
-            ? "#f97316"
-            : "#0ea5e9"
-        }`;
-        const textColor = pathColor;
-        return (
+      {data.map((item, index) => (
           <Item asChild value={`item-${index}`} key={index}>
             <Header className="rounded-md border border-slate-300/80">
               <Trigger className="grid w-full grid-cols-[1fr_auto_auto] gap-x-4 p-4 text-left items-center">
                 <span>{item.filename}</span>
-                <div className="w-12">
-                  <CircularProgressbar
-                    minValue={0}
-                    maxValue={100}
-                    text={`${item.result.aiGeneratedPercentage}%`}
-                    value={value}
-                    styles={buildStyles({
-                      pathColor: pathColor,
-                      textColor: textColor,
-                    })}
-                  />
-                </div>
+                <CircularProgress value={item.result.aiGeneratedPercentage} />
                 <FaChevronDown size={16} />
               </Trigger>
               <Content>Hello World</Content>
             </Header>
           </Item>
-        );
-      })}
+        )
+      )}
     </Root>
   );
 };
