cbea531b64a14599297b6f5c9dc9b741436f7690
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-26 21:59:25 +0800

    rename and stable implementation

---
+++ src/utils/createDetectionReportDocx.ts
@@ -0,0 +1,264 @@
+import { ResultWithFilename } from "@/types";
+import {
+  Document,
+  Paragraph,
+  TextRun,
+  Header,
+  Table,
+  TableRow,
+  TableCell,
+  WidthType,
+  TabStopType,
+  TabStopPosition,
+  VerticalAlign,
+  BorderStyle,
+  AlignmentType,
+} from "docx";
+
+const TableCellMargin = 100;
+const TableSize = 100;
+
+const documentStyles = {
+  paragraphStyles: [
+    {
+      id: "Normal",
+      name: "Normal",
+      basedOn: "Normal",
+      next: "Normal",
+      quickFormat: true,
+      run: {
+        font: "Segoe UI",
+      },
+    },
+  ],
+};
+
+const headerChildren = new Paragraph({
+  children: [
+    new TextRun({
+      text: "VerityAI | Detection Report",
+      font: "Product Sans",
+      bold: true,
+    }),
+    new TextRun({
+      text: "\t" + new Date().toLocaleDateString(),
+      font: "Product Sans",      
+    }),
+  ],
+  tabStops: [
+    {
+      type: TabStopType.RIGHT,
+      position: TabStopPosition.MAX,
+    },
+  ],
+});
+
+const header = {
+  default: new Header({
+    children: [headerChildren],
+  }),
+};
+
+const tableHeadingTitles = [
+  "Filenames",
+  "Overall Percentage of AI-Generated Text",
+  "Detection Result Breakdown",
+];
+const tableHeading = tableHeadingTitles.map(
+  (title) =>
+    new TableCell({
+      verticalAlign: VerticalAlign.CENTER,
+      children: [
+        new Paragraph({
+          children: [
+            new TextRun({
+              text: title,
+              font: "Product Sans",
+              bold: true,
+            }),
+          ],
+          alignment: AlignmentType.CENTER,
+        }),
+      ],
+    })
+);
+
+const innerTableHeadingTitles = ["Paragraphs", "Results"];
+const innerTableHeading = innerTableHeadingTitles.map(
+  (title) =>
+    new TableCell({
+      verticalAlign: VerticalAlign.CENTER,
+      children: [
+        new Paragraph({
+          children: [
+            new TextRun({
+              text: title,
+              font: "Product Sans",
+              bold: true,
+            }),
+          ],
+          alignment: AlignmentType.CENTER,
+        }),
+      ],
+      margins: {
+        bottom: TableCellMargin,
+      }
+    })
+);
+
+const createDetectionReportDocx = (data: ResultWithFilename[]) => {
+  const detectionResultRow = data.map(({filename, result}) => {
+    const tableRow = new TableRow({
+      children: [
+        new TableCell({
+          children: [
+            new Paragraph({
+              text: filename,
+              alignment: AlignmentType.CENTER,
+            })
+          ],
+          margins: {
+            top: TableCellMargin,
+            bottom: TableCellMargin,
+            left: TableCellMargin,
+            right: TableCellMargin,
+          },
+        }),
+
+        new TableCell({
+          children: [
+            new Paragraph({
+              children: [
+                new TextRun({
+                  text: `${result.aiGeneratedPercentage}%`
+                })
+              ],
+              alignment: AlignmentType.CENTER,
+            })
+          ],
+        }),
+      ]
+    })
+
+    if (typeof result.aiGeneratedTexts === "string") {
+      tableRow.addChildElement(
+        new TableCell({
+          children: [
+            new Paragraph({
+              children: [
+                new TextRun({
+                  text: `${result.aiGeneratedTexts}`,
+                  bold: true,
+                })
+              ]
+            })
+          ],
+          margins: {
+            left: TableCellMargin,
+          },
+          verticalAlign: VerticalAlign.CENTER
+        })
+      )
+    }
+
+    else {
+      const breakdownResultRow = result.aiGeneratedTexts.map(({text, label, score}) => {
+        const textLabel =
+          label === "LABEL_0" ? "AI-Generated" : "Human Written";
+        return new TableRow({
+          children: [
+            new TableCell({
+              children: [new Paragraph({
+                text: text,
+                alignment: AlignmentType.JUSTIFIED,
+              })],
+              margins: {
+                top: TableCellMargin,
+                bottom: TableCellMargin,
+                left: TableCellMargin,
+                right: TableCellMargin,
+              },
+            }),
+            new TableCell({
+              children: [new Paragraph({
+                children: [
+                  new TextRun({
+                    text: `${score}% ${textLabel}`,
+                    bold: true,
+                  })
+                ],
+                alignment: AlignmentType.CENTER,
+              })],
+              verticalAlign: VerticalAlign.CENTER
+            }),
+          ],
+        });
+      });
+  
+      const innerTable = new Table({
+        width: {
+          size: TableSize,
+          type: WidthType.PERCENTAGE,
+        },
+        rows: [
+          new TableRow({
+            children: innerTableHeading
+          }),
+          ...breakdownResultRow,
+        ],
+        borders: {
+          top: { size: 0, style: BorderStyle.NONE },
+          bottom: { size: 0, style: BorderStyle.NONE },
+          left: { size: 0, style: BorderStyle.NONE },
+          right: { size: 0, style: BorderStyle.NONE },
+        },
+        margins: {
+          top: 0,
+          bottom: 0,
+        }
+      });
+
+      tableRow.addChildElement(
+        new TableCell({
+          children: [innerTable],
+          margins: {
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          },
+        })
+      )
+    }
+
+    return tableRow;
+  });
+
+  const generatedDocx = new Document({
+    styles: documentStyles,
+    sections: [
+      {
+        properties: {},
+        headers: header,
+        children: [
+          new Table({
+            width: {
+              size: TableSize,
+              type: WidthType.PERCENTAGE,
+            },
+            rows: [
+              new TableRow({
+                children: tableHeading,
+              }),
+              ...detectionResultRow,
+            ],
+          }),
+        ],
+      },
+    ],
+  });
+
+  return generatedDocx;
+};
+
+export default createDetectionReportDocx;
