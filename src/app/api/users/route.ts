b879df74fbeec5c228796b2a718716597fa27565
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-27 20:37:20 +0800

    rename

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,6 +4,10 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const users = await prisma.name.findMany();
+  const users = await prisma.user.findMany({
+    include: {
+      name: true,
+    }
+  });
   return NextResponse.json(users);
 }
