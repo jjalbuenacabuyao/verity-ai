384679dfce32ba24453b72483afbbde631fbc4d3
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-27 20:37:57 +0800

    fetch data using axios

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,32 @@
-"use client";
+"use client"
 
-import { Header, AddUserModal } from "@/components";
+import { AddUserModal } from "@/components";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { UserType } from "@/types";
 
 const Dashboard = () => {
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  async function getAllUsers() {
+    const fetchedUsers: UserType[] = await axios("/api/users").then(res => res.data);
+    setUsers(fetchedUsers);
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, [])
+
   return (
     <div>
       <div>
-        <h1>Users</h1>
-        <button>+ Add New User</button>
+        <h1>Dashboard</h1>
+        {users.map(user => (
+          <div key={user.id}>
+            <p>{user.name.firstName} {user.name.middleName} {user.name.lastName}</p>
+            <p>{user.email} {user.role}</p>
+          </div>
+        ))}
       </div>
       <AddUserModal />
     </div>
