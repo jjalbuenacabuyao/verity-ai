a9b3ded11e7e86832d6d2f20f865d4b4bff10cc9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-31 21:29:56 +0800

    add table styles

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -6,20 +6,28 @@ interface Props {
 }
 
 const UserTable = ({ users }: Props) => {
+  const tableHeadings = ["Name", "Role", "Actions"];
+
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Role</th>
-        <th>Actions</th>
-      </tr>
-      {users.map((user) => (
-        <tr key={user.id}>
-          <td>{`${user.name.firstName} ${user.name.middleName} ${user.name.lastName}`}</td>
-          <td>{user.role}</td>
-        </tr>
-      ))}
-    </table>
+    <div className="relative rounded-xl overflow-x-auto border pb-8">
+      <table className="table-auto w-full text-sm text-slate-500">
+        <thead>
+          <tr className="text-left border-b">
+            {tableHeadings.map(heading => (
+              <td key={heading} className="p-8 pb-4 font-medium text-slate-400">{heading}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id} className="border-b border-b-gray-100">
+              <td className="px-8 py-4 capitalize">{`${user.name.firstName} ${user.name.middleName} ${user.name.lastName}`}</td>
+              <td className="px-8 py-4">{user.role.charAt(0) + user.role.substring(1).toLowerCase()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
