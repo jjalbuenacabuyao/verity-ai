bdcd821e298f3dbae614b5e6fc1604ae5dad1cdf
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-07-31 21:28:17 +0800

    add inter

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,7 +1,13 @@
-import { Work_Sans } from "next/font/google";
+import { Inter, Work_Sans } from "next/font/google";
 
 export const workSans = Work_Sans({
   subsets: ["latin"],
   display: "swap",
   weight: ["700", "900"]
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
 });
