53d5c1f8bc25cb51169fb8895ad77d112caf3d4e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-03 06:24:55 +0800

    add dependency

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -12,7 +12,7 @@ const Detector = () => {
 
   useEffect(() => {
     router.refresh();
-  })
+  }, [router])
 
   return (
     <div className="mx-6 pt-24 lg:pt-28 lg:mx-16 lg:grid lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:h-screen lg:max-h-screen lg:overflow-hidden">
