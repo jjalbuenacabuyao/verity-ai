5918e271b144fcba2c01aa5cf74d294462011cd6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-03 07:29:33 +0800

    commented the useEffect

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -8,11 +8,11 @@ import { useRouter } from "next/navigation";
 const Detector = () => {
   const [files, setFiles] = useState<File[]>([]);
   const currentUser = useCurrentUserContext();
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.refresh();
-  }, [router])
+  // useEffect(() => {
+  //   router.refresh();
+  // }, [router])
 
   return (
     <div className="mx-6 pt-24 lg:pt-28 lg:mx-16 lg:grid lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:h-screen lg:max-h-screen lg:overflow-hidden">
