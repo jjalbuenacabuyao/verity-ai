a1f5e643396bc29173776bad1dea1f6de8179408
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-03 08:05:00 +0800

    remove useEffect

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Aside, ResultContainer } from "@/components";
 import { useCurrentUserContext } from "@/hooks/userContext";
-import { useRouter } from "next/navigation";
 
 const Detector = () => {
   const [files, setFiles] = useState<File[]>([]);
   const currentUser = useCurrentUserContext();
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   router.refresh();
-  // }, [router])
 
   return (
     <div className="mx-6 pt-24 lg:pt-28 lg:mx-16 lg:grid lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:h-screen lg:max-h-screen lg:overflow-hidden">
