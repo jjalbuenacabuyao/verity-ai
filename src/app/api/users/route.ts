3ae4f0dca230d6fcd1872d8cbc088336962fad87
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-04 22:14:30 +0800

    initial commit

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,13 +1,27 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export async function GET() {
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+
+  const page = Number(url.searchParams.get("page"));
+
+  const skip = (page - 1) * 4;
+  
   const users = await prisma.user.findMany({
+    take: 4,
+    skip,
+    orderBy: {
+      id: 'asc',
+    },
     include: {
       name: true,
     }
-  });
-  return NextResponse.json(users);
+  })
+  if (users.length === 0) {
+    return NextResponse.json(null)
+  }
+  return NextResponse.json(users)
 }
