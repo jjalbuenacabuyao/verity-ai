6b19d87d646fecdaaa51a0b1a3546d41316189f1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-04 22:16:23 +0800

    check if name is not null

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,30 +1,41 @@
 import { UserType } from "@/types";
+import { Name, User } from "@prisma/client";
 import React from "react";
 
 interface Props {
-  users: UserType[];
+  users: (User & {
+    name: Name | null;
+  })[];
 }
 
 const UserTable = ({ users }: Props) => {
   const tableHeadings = ["Name", "Role", "Actions"];
 
   return (
-    <div className="relative rounded-xl overflow-x-auto border border-slate-400 pb-8">
-      <table className="table-auto w-full text-sm text-slate-500">
+    <div className="relative overflow-x-auto rounded-xl border border-slate-400 pb-8">
+      <table className="w-full table-auto text-sm text-slate-600">
         <thead>
-          <tr className="text-left border-b border-b-slate-400">
-            {tableHeadings.map(heading => (
-              <td key={heading} className="p-8 pb-4 font-medium text-slate-400">{heading}</td>
+          <tr className="border-b border-b-slate-400 text-left">
+            {tableHeadings.map((heading) => (
+              <td key={heading} className="p-8 pb-4 font-medium text-slate-500">
+                {heading}
+              </td>
             ))}
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
-            <tr key={user.id} className="border-b border-b-gray-300">
-              <td className="px-8 py-4 capitalize">{`${user.name.firstName} ${user.name.middleName} ${user.name.lastName}`}</td>
-              <td className="px-8 py-4">{user.role.charAt(0) + user.role.substring(1).toLowerCase()}</td>
-            </tr>
-          ))}
+          {users.map((user) => {
+            if (user.name) {
+              return (
+                <tr key={user.id} className="border-b border-b-gray-300">
+                  <td className="px-8 py-4 capitalize">{`${user.name.firstName} ${user.name.middleName} ${user.name.lastName}`}</td>
+                  <td className="px-8 py-4">
+                    {user.role.charAt(0) + user.role.substring(1).toLowerCase()}
+                  </td>
+                </tr>
+              );
+            }
+          })}
         </tbody>
       </table>
     </div>
