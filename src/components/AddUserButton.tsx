7e0ec3cd2d9221c1b4b650df6a6fb888feeca7de
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-07 08:54:21 +0800

    add open state

--- src/components/AddUserButton.tsx
+++ src/components/AddUserButton.tsx
@@ -9,12 +9,13 @@ interface Props {
 }
 
 const AddUserButton = ({ setUserAdded }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <Root>
+    <Root open={isOpen} onOpenChange={setIsOpen}>
       <Trigger asChild>
         <Button variant="primary" text="Add User" />
       </Trigger>
-      <AddUserModal setUserAdded={setUserAdded} />
+      <AddUserModal setUserAdded={setUserAdded} setIsOpen={setIsOpen} />
     </Root>
   )
 }
