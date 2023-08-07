25a6eb9f25a7f7a1f1f035e1389440f18c3e7755
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-07 15:04:07 +0800

    add idle timer

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -3,10 +3,22 @@
 import React, { useState } from "react";
 import { Aside, ResultContainer } from "@/components";
 import { useCurrentUserContext } from "@/hooks/userContext";
+import { useRouter } from "next/navigation";
+import { useIdleTimer } from "react-idle-timer";
 
 const Detector = () => {
   const [files, setFiles] = useState<File[]>([]);
   const currentUser = useCurrentUserContext();
+  const router = useRouter();
+
+  const onIdle = () => {
+    router.refresh();
+  };
+
+  useIdleTimer({
+    onIdle,
+    timeout: 1000 * 60 * 30, // 30 minutes
+  });
 
   return (
     <div className="mx-6 pt-24 lg:pt-28 lg:mx-16 lg:grid lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:h-screen lg:max-h-screen lg:overflow-hidden">
