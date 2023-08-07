2a58ebf2ea6d3502cf01b9a200d7ec13161f9064
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-07 22:40:34 +0800

    add nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+const { nextui } = require("@nextui-org/react");
+
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     supports: {
@@ -65,5 +68,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }
