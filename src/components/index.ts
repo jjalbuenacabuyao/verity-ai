c02051e05ed312d1da172de373b2688bd4695bb9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-08 22:20:08 +0800

    export ResultAccordion

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import LogInModal from "./LogInModal";
 import Logo from "./Logo";
+import ResultsAccordion from "./ResultsAccordion";
 import ResultContainer from "./ResultContainer";
 import Searchbar from "./Searchbar";
 import TableSkeleton from "./TableSkeleton";
@@ -18,6 +19,7 @@ export {
   Hero,
   LogInModal,
   Logo,
+  ResultsAccordion,
   ResultContainer,
   Searchbar,
   TableSkeleton,
