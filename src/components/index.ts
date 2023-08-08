cf561433e077515b67530a8bb411b1453fc494bb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-08 14:09:20 +0800

    remove LoginButton and Nav components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,8 @@ import AddUserModal from "./AddUserModal";
 import Aside from "./Aside";
 import Header from "./Header";
 import Hero from "./Hero";
-import LogInButton from "./LogInButton";
 import LogInModal from "./LogInModal";
 import Logo from "./Logo";
-import Nav from "./Nav";
 import ResultContainer from "./ResultContainer";
 import Searchbar from "./Searchbar";
 import TableSkeleton from "./TableSkeleton";
@@ -18,10 +16,8 @@ export {
   Aside,
   Header,
   Hero,
-  LogInButton,
   LogInModal,
   Logo,
-  Nav,
   ResultContainer,
   Searchbar,
   TableSkeleton,
