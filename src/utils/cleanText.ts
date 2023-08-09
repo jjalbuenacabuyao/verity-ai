257170c71ab7401eb0c896dc2484d035747a45d2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-09 22:08:00 +0800

    remove a comment

--- src/utils/cleanText.ts
+++ src/utils/cleanText.ts
@@ -2,7 +2,6 @@ export default function cleanText(text: string): string {
   const trimmedText: string = text.trim();
   const normalizedString: string = trimmedText
     .replace(/(\r\n|\n|\r)/gm, " ")
-    // .replace(/\n+/g, '\n')
     .replace(/ +/g, ' ');
     // .replace(/[ ]{2,}/gi, " ");
   return normalizedString;
