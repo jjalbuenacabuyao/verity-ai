e1650fa6ff7289a09d96a90aa4156675f0a257aa
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-10 10:18:05 +0800

    initial commit

---
+++ src/components/AccordionItemSectionTitle.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DiTerminalBadge } from "react-icons/di";
+
+interface Props {
+  title: string;
+  description: string;
+};
+
+const AccordionItemSectionTitle = ({ title, description }: Props) => {
+  return (
+    <div className="mb-3 grid grid-cols-[auto_1fr] gap-2">
+      <DiTerminalBadge size={24} />
+      <div>
+        <p className="font-medium">{title}</p>
+        <p className="text-sm">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItemSectionTitle;
