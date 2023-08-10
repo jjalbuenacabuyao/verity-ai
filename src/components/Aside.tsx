b3cab7114f1fd856ede68d0777b4a06a607ce667
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-10 17:33:25 +0800

    add animation

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -19,24 +19,37 @@ const Aside = ({ setFiles }: Props) => {
   };
 
   return (
-    <aside className="mb-16 pt-8 lg:pt-10 lg:mb-0">
-      <div className="flex flex-col items-center rounded-lg border-2 border-gray-300 border-dashed py-6 lg:sticky lg:top-28 lg:inset-x-auto">
-        <FiUpload size={36} />
-        <h1 className="mb-6 mt-4">Upload pdf and docx files</h1>
-        <label
-          tabIndex={17}
-          htmlFor="file"
-          className="flex items-center gap-2 rounded-full bg-blue-600 px-6 py-2 text-sm tracking-wide text-white font-medium cursor-pointer hover:shadow-md hover:bg-blue-800 hover:shadow-slate-400 transition-all">
-          Browse Files
-          <input
-            className="hidden"
-            id="file"
-            type="file"
-            onChange={handleFileChange}
-            multiple
-            accept=".doc,.docx,.pdf,application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-          />
-        </label>
+    <aside className="mb-16 pt-8 lg:mb-0 lg:pt-10">
+      <div className="flex flex-col gap-4 lg:sticky lg:inset-x-auto lg:top-28">
+        <div className="group flex flex-col items-center rounded-lg border-2 border-dashed border-gray-300 py-6">
+          <FiUpload size={36} className="group-hover:animate-bounce" />
+          <h1 className="mb-6 mt-4">Upload pdf and docx files</h1>
+          <label
+            htmlFor="file"
+            className="flex cursor-pointer items-center gap-2 rounded-full bg-blue-600 px-6 py-2 text-sm font-medium tracking-wide text-white transition-all hover:bg-blue-800 hover:shadow-md hover:shadow-slate-400">
+            Browse Files
+            <input
+              className="hidden"
+              id="file"
+              type="file"
+              onChange={handleFileChange}
+              multiple
+              accept=".doc,.docx,.pdf,application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            />
+          </label>
+        </div>
+
+        <div className="p-3 shadow-lg border rounded-lg">
+          <p className="text-sm">
+            <span className="font-medium">Important 🚨:</span> You can upload maximum of <span className="font-medium">20</span> files each time.
+          </p>
+        </div>
+
+        <div className="p-3 shadow-lg border rounded-lg">
+          <p className="text-sm">
+            <span className="font-medium">Important 🚨:</span> Only <span className="font-medium">docx and pdf</span> files are supported.
+          </p>
+        </div>
       </div>
     </aside>
   );
