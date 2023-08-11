dfd5e9f4ab07f1b9bbd40eb5f6e50bfa05eb5104
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-11 21:26:14 +0800

    add toast, onclose and setUserAdded

--- src/components/AddUserModal.tsx
+++ src/components/AddUserModal.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import React, {
-  ChangeEvent,
-  FormEvent,
-  useState,
-} from "react";
+import React, { ChangeEvent, Dispatch, FormEvent, SetStateAction, useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import {
   Modal,
   ModalBody,
@@ -19,6 +14,7 @@ import { Button } from "@nextui-org/button";
 import { Radio, RadioGroup } from "@nextui-org/radio";
 import EyeFilledIcon from "./EyeFilledIcon";
 import EyeSlashFilledIcon from "./EyeSlashFilledIcon";
+import UserAddedToast from "./UserAddedToast";
 
 interface User {
   email: string;
@@ -31,10 +27,13 @@ interface User {
 
 interface Props {
   isOpen: boolean;
+  userAdded: boolean;
   onOpenChange: () => void;
+  onClose: () => void;
+  setUserAdded: Dispatch<SetStateAction<boolean>>;
 }
 
-const AddUserModal = ({ isOpen, onOpenChange }: Props) => {
+const AddUserModal = ({ isOpen, onOpenChange, setUserAdded, userAdded, onClose }: Props) => {
   const [user, setUser] = useState<User>({
     email: "",
     password: "",
@@ -45,8 +44,6 @@ const AddUserModal = ({ isOpen, onOpenChange }: Props) => {
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  const router = useRouter();
 
   const toggleVisibility = () => setIsVisible(!isVisible);
 
@@ -62,106 +59,118 @@ const AddUserModal = ({ isOpen, onOpenChange }: Props) => {
 
     if (response.statusText === "OK") {
       setIsLoading(false);
+      setUserAdded(true);
+      onClose();
     }
   };
 
   const nameInputFields = ["firstname", "middlename", "lastname"];
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onOpenChange={onOpenChange}
-      placement="center"
-      scrollBehavior="inside">
-      <form onSubmit={handleSubmit}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Add user
-              </ModalHeader>
-              <ModalBody className="gap-5">
-                <fieldset className="grid gap-3 capitalize">
-                  <p className="font-semibold">Name</p>
-                  {nameInputFields.map((field) => (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+        scrollBehavior="inside">
+        <form onSubmit={handleSubmit}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Add user
+                </ModalHeader>
+                <ModalBody className="gap-7">
+                  <fieldset className="grid gap-3 capitalize">
+                    <p className="font-semibold">Name</p>
+                    {nameInputFields.map((field) => (
+                      <Input
+                        key={field}
+                        autoFocus={field === "firstname"}
+                        label={field === "middlename" ? "Middle name" : field}
+                        name={field}
+                        variant="bordered"
+                        isRequired
+                        onChange={handleChange}
+                      />
+                    ))}
+                  </fieldset>
+                  <fieldset className="grid gap-3">
+                    <p className="font-semibold">Role</p>
+                    <RadioGroup
+                      defaultValue="USER"
+                      orientation="horizontal"
+                      name="role"
+                      //@ts-ignore
+                      onChange={handleChange}>
+                      <Radio value="USER" className="rounded-lg border py-2 px-4">
+                        <p className="font-semibold">User</p>
+                        <ul className="text-xs">
+                          <li>✅ AI-Detector</li>
+                          <li>❌ Dashboard</li>
+                        </ul>
+                      </Radio>
+                      <Radio
+                        value="ADMIN"
+                        className="ml-4 rounded-lg border py-2 px-4">
+                        <p className="font-semibold">Admin</p>
+                        <ul className="text-xs">
+                          <li>✅ AI-Detector</li>
+                          <li>✅ Dashboard</li>
+                        </ul>
+                      </Radio>
+                    </RadioGroup>
+                  </fieldset>
+                  <fieldset className="grid gap-3">
+                    <p className="font-semibold">Email and Password</p>
                     <Input
-                      key={field}
-                      autoFocus={field === "firstname"}
-                      label={field === "middlename" ? "Middle name" : field}
-                      name={field}
+                      label="Email"
+                      name="email"
                       variant="bordered"
                       isRequired
                       onChange={handleChange}
                     />
-                  ))}
-                </fieldset>
-                <fieldset className="grid gap-3">
-                  <p className="font-semibold">Role</p>
-                  <RadioGroup
-                    defaultValue="USER"
-                    orientation="horizontal"
-                    name="role"
-                    //@ts-ignore
-                    onChange={handleChange}>
-                    <Radio value="USER" className="p-2 border rounded-lg">
-                      <p className="font-semibold">User</p>
-                      <ul className="text-xs">
-                        <li>✅ AI-Detector</li>
-                        <li>❌ Dashboard</li>
-                      </ul>
-                    </Radio>
-                    <Radio value="ADMIN" className="ml-4 p-2 border rounded-lg">
-                    <p className="font-semibold">Admin</p>
-                      <ul className="text-xs">
-                        <li>✅ AI-Detector</li>
-                        <li>✅ Dashboard</li>
-                      </ul>
-                    </Radio>
-                  </RadioGroup>
-                </fieldset>
-                <fieldset className="grid gap-3">
-                  <p className="font-semibold">Email and Password</p>
-                  <Input
-                    label="Email"
-                    name="email"
-                    variant="bordered"
-                    isRequired
-                    onChange={handleChange}
-                  />
-                  <Input
-                    label="Password"
-                    variant="bordered"
-                    name="password"
-                    isRequired
-                    endContent={
-                      <button
-                        className="focus:outline-none"
-                        type="button"
-                        onClick={toggleVisibility}>
-                        {isVisible ? (
-                          <EyeSlashFilledIcon className="pointer-events-none text-2xl text-default-400" />
-                        ) : (
-                          <EyeFilledIcon className="pointer-events-none text-2xl text-default-400" />
-                        )}
-                      </button>
-                    }
-                    type={isVisible ? "text" : "password"}
-                  />
-                </fieldset>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onClick={onClose}>
-                  Close
-                </Button>
-                <Button isLoading={isLoading} color="primary" type="submit">
-                  Create user
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </form>
-    </Modal>
+                    <Input
+                      label="Password"
+                      variant="bordered"
+                      name="password"
+                      isRequired
+                      endContent={
+                        <button
+                          className="focus:outline-none"
+                          type="button"
+                          onClick={toggleVisibility}>
+                          {isVisible ? (
+                            <EyeSlashFilledIcon className="pointer-events-none text-2xl text-default-400" />
+                          ) : (
+                            <EyeFilledIcon className="pointer-events-none text-2xl text-default-400" />
+                          )}
+                        </button>
+                      }
+                      type={isVisible ? "text" : "password"}
+                    />
+                  </fieldset>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="flat" onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button isLoading={isLoading} color="primary" type="submit">
+                    Create user
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </form>
+      </Modal>
+      {userAdded && (
+        <UserAddedToast
+          userAdded={userAdded}
+          setUserAdded={setUserAdded}
+        />
+      )}
+    </>
   );
 };
 
