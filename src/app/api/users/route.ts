ba4c764a3eb4edb2d40e325dd6d9171642985a09
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-14 16:57:42 +0800

    search the user if the search value is not empty

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,13 +5,51 @@ const prisma = new PrismaClient()
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
-
   const usersPerPage = 5;
-
   const page = Number(url.searchParams.get("page"));
-
+  const search = url.searchParams.get("search") as string;
   const skip = (page - 1) * usersPerPage;
   
+  if (search !== "") {
+    const searchResult = await prisma.user.findMany({
+      take: usersPerPage,
+      skip,
+      orderBy: {
+        name: {
+          firstName: "asc",
+        }
+      },
+      where: {
+        name: {
+          OR: [
+            {
+              firstName: {
+                contains: search,
+                mode: "insensitive",
+              }
+            },
+            {
+              middleName: {
+                contains: search,
+                mode: "insensitive",
+              }
+            },
+            {
+              lastName: {
+                contains: search,
+                mode: "insensitive",
+              }
+            },
+          ],
+        },
+      },
+      include: {
+        name: true,
+      }
+    })
+    return NextResponse.json(searchResult)
+  }
+
   const users = await prisma.user.findMany({
     take: usersPerPage,
     skip,
@@ -24,8 +62,5 @@ export async function GET(req: Request) {
       name: true,
     }
   })
-  if (users.length === 0) {
-    return NextResponse.json(null)
-  }
   return NextResponse.json(users)
 }
