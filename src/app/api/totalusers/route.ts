61f7d69e86e4793a0ce115b3dbec571eff845bca
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-15 20:46:17 +0800

    disable caching

--- src/app/api/totalusers/route.ts
+++ src/app/api/totalusers/route.ts
@@ -5,5 +5,9 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   const users = await prisma.user.findMany();
-  return NextResponse.json(users.length);
+  return NextResponse.json(users.length, {
+    headers: {
+      "Cache-Control": "no-store",
+    },
+  });
 }
