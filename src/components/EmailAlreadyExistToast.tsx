687bdaa7c84475952e338fa7a0838e2eb7c0cea2
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-16 21:17:19 +0800

    remove border

--- src/components/EmailAlreadyExistToast.tsx
+++ src/components/EmailAlreadyExistToast.tsx
@@ -14,7 +14,7 @@ const EmailAlreadyExistToast = ({ isError, setIsError }: Props) => {
         duration={8000}
         open={isError}
         onOpenChange={setIsError}
-        className='flex items-center gap-4 text-white p-5 border bg-red-500 rounded-md shadow-lg data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut'>
+        className='flex items-center gap-4 text-white p-5 bg-red-500 rounded-md shadow-lg data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut'>
         <Description className='max-w-md text-sm'>The email address you entered is already associated with an existing account.</Description>
         <Close>
           <AiFillCloseCircle size={20} />
