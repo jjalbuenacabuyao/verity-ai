4147065e5a9eefe21c914fd7ded93b5d2caad6ee
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-16 21:30:23 +0800

    initial commit

---
+++ src/components/PasswordVisibilityToggler.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import EyeSlashFilledIcon from "./EyeSlashFilledIcon";
+import EyeFilledIcon from "./EyeFilledIcon";
+
+interface Props {
+  toggleVisibility: () => void;
+  isVisible: boolean;
+}
+
+const PasswordVisibilityToggler = ({ toggleVisibility, isVisible }: Props) => {
+  return (
+    <button
+      className="focus:outline-none"
+      type="button"
+      onClick={toggleVisibility}>
+      {isVisible ? (
+        <EyeSlashFilledIcon className="pointer-events-none text-2xl text-default-400" />
+      ) : (
+        <EyeFilledIcon className="pointer-events-none text-2xl text-default-400" />
+      )}
+    </button>
+  );
+};
+
+export default PasswordVisibilityToggler;
