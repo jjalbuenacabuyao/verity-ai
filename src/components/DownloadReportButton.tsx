af6f28392f226276ddbfa8f695e961d43e900e5f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-19 14:27:26 +0800

    use nextui Button

--- src/components/DownloadReportButton.tsx
+++ src/components/DownloadReportButton.tsx
@@ -1,8 +1,8 @@
 import { ResultWithFilename } from "@/types";
 import { createDetectionReportDocx } from "@/utils";
 import { saveAs } from "file-saver";
-import Button from "./Button";
 import { Packer } from "docx";
+import { Button } from "@nextui-org/button";
 
 interface Props {
   results: ResultWithFilename[];
@@ -22,7 +22,7 @@ const DownloadReportButton = ({ results, isLoading }: Props) => {
   const second = date.getSeconds();
 
   const dateString = `${year}-${month}-${dateToday}`;
-  const filename = `Detection Report - (${dateString})_${hour}${minute}${second}.docx`
+  const filename = `Detection Report - (${dateString})_${hour}${minute}${second}.docx`;
 
   async function downloadDocx() {
     const buffer = await Packer.toBlob(generatedDocx);
@@ -30,11 +30,15 @@ const DownloadReportButton = ({ results, isLoading }: Props) => {
   }
   return (
     <Button
-      variant="secondary"
-      text="Download Report"
-      onClick={downloadDocx}
-      disabled={results.length === 0 || isLoading ? true : false}
-    />
+      color="primary"
+      variant="bordered"
+      radius="full"
+      isDisabled={results.length === 0 || isLoading}
+      onPress={downloadDocx}
+      className="text-xs tracking-wide font-medium hover:shadow-md"
+    >
+      Download Report
+    </Button>
   );
 };
 
