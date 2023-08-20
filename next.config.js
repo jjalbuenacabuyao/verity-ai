c805fbb8f974328e24e400ce1e223abae81e5bb6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-20 16:35:59 +0800

    enables experimental serverActions

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config, options) => {
-  //   config.module.rules.push({
-  //     test: /\.(png|svg|jpg|gif|pdf)$/,
-  //     use: [
-  //       {
-  //         loader: "file-loader",
-  //         options: {
-  //           name: "[name].[ext]"
-  //         }
-  //       }
-  //     ]
-  //   });
-
-  //   config.resolve.modules.push(__dirname);
-
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias
-  //   };
-
-  //   return config;
-  // },
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig
