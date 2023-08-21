e5d90f288f8ca2acacf51cc488bbf4f32bccad8c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-08-21 13:45:11 +0800

    check if there is a session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,17 @@
+import AccessDenied from "@/components/AccessDenied";
+import { getSession } from "../actions/getCurrentUser";
 import client from "../libs/prismadb";
 import DashboardContents from "@/components/DashboardContents";
 
 const Dashboard = async () => {
+  const session = await getSession();
   const totalUsers = (await client.user.findMany()).length;
+
+  if (!session) {
+    return (
+      <AccessDenied />
+    )
+  }
 
   return (
     <div className="mx-6 pb-10 pt-8 lg:mx-16 lg:pt-10">
