f88ae44782cf2e82ce2f49d99c48f7c0153dccb7
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-23 15:05:35 +0800

    add illustration

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,29 @@ import { workSans } from "@/fonts";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center px-6 py-24 text-center">
-      <Image src={"/logo.svg"} width={200} height={200} alt="Verity" />
+    <div className="flex flex-col items-center px-6 text-center lg:grid lg:grid-cols-2">
       <div className="grid gap-4">
         <h1
-          className={`${workSans.className} bg-gradient-to-tr from-light-violet to-light-blue bg-clip-text  text-2xl font-black text-transparent`}>
+          className={`${workSans.className} bg-gradient-to-tr from-light-violet to-light-blue bg-clip-text  text-2xl font-black text-transparent`}
+        >
           VerityAI
         </h1>
-        <p className="text-2xl font-bold">Detect AI-Generated Academic Submissions</p>
+        <p className="text-2xl font-bold">
+          Detect AI-Generated Academic Submissions
+        </p>
+      </div>
+
+      <div className="relative">
+        <Image
+          src={"/home-illustration.png"}
+          width={100}
+          height={100}
+          quality={100}
+          unoptimized
+          priority
+          alt="Photo of AI-detector"
+          className="h-auto w-full border shadow-md rounded-lg"
+        />
       </div>
     </div>
   );
