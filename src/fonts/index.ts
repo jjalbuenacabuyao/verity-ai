45ba8ab961ad579cc9a459375d703f7ed08c44d3
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-23 21:07:39 +0800

    add font-weight 600

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -3,7 +3,7 @@ import { Inter, Work_Sans } from "next/font/google";
 export const workSans = Work_Sans({
   subsets: ["latin"],
   display: "swap",
-  weight: ["700", "900"]
+  weight: ["600", "700", "900"]
 });
 
 export const inter = Inter({
