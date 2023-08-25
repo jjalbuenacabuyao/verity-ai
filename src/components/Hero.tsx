618bacdf926805db725fed1cb43d5eedda68acbf
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-25 21:10:01 +0800

    add detection report photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,28 +6,43 @@ import { CircularProgress } from "@nextui-org/progress";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center gap-12 px-6 lg:grid lg:grid-cols-2">
+    <div className="relative flex flex-col items-center gap-24 px-6 lg:grid lg:grid-cols-2">
+      <div className="absolute bottom-6 left-1/2 right-6 rounded-lg shadow-lg shadow-slate-300 overflow-hidden">
+        <Image
+          src={"/detection-report.png"}
+          width={100}
+          height={100}
+          quality={100}
+          unoptimized
+          priority
+          alt="Photo of AI-detector"
+          className="static h-auto w-full"
+        />
+      </div>
+
       <div className="grid gap-4 pt-6">
         <h1
-          className={`${workSans.className} text-4xl font-semibold tracking-[-0.25px] text-slate-900`}>
+          className={`${workSans.className} text-4xl font-semibold tracking-[-0.25px] text-slate-900`}
+        >
           Advanced AI Content Detector For The Academe
         </h1>
         <p className={`${inter.className}`}>
-          Detect Artificial Intelligence(AI) generated texts from academic
+          Educator's partner in detecting Artificial Intelligence(AI) generated texts from academic
           submissions using state-of-the-art Large Language Model(LLM).
         </p>
         <Button
           color="primary"
           radius="sm"
           size="lg"
-          className="font-semibold hover:bg-blue-800 hover:shadow-lg">
+          className="mt-4 font-semibold hover:bg-blue-800 hover:shadow-lg"
+        >
           Get Started
         </Button>
       </div>
 
       <div className="relative pb-10">
-        <div className="grid grid-cols-[1fr_auto] gap-2">
-          <div>
+        <div className="grid grid-cols-[auto_auto] gap-2">
+          <div className="rounded-lg border shadow-lg overflow-hidden">
             <Image
               src={"/home-illustration.png"}
               width={100}
@@ -36,16 +51,16 @@ const Hero = () => {
               unoptimized
               priority
               alt="Photo of AI-detector"
-              className="h-auto w-full rounded-lg border shadow-md"
+              className="h-auto w-full"
             />
           </div>
-          <div className="flex flex-col justify-center gap-2">
+          <div className="flex flex-col justify-center gap-2 pr-2">
             {["/pdf.png", "/docx-file.png"].map((imgSource, index) => (
               <Image
                 key={imgSource}
                 src={imgSource}
-                width={20}
-                height={24}
+                width={28}
+                height={36}
                 quality={100}
                 unoptimized
                 priority
@@ -55,7 +70,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="relative mt-2 grid grid-cols-[auto_auto_1fr] gap-8">
+        <div className="relative mt-2 grid grid-cols-[auto_auto_1fr] gap-6">
           <div className="flex items-center gap-1 pl-4">
             {["JC", "NO", "KN"].map((initials) => (
               <Avatar
@@ -76,16 +91,17 @@ const Hero = () => {
               variant="bordered"
               color="primary"
               radius="full"
-              className="h-6 px-2 text-[6px]">
+              className="h-6 px-2 text-[6px] leading-none"
+            >
               Download Report
             </Button>
           </div>
 
-          <div className="absolute bottom-4 left-[60%] right-0 flex justify-evenly rounded-md border bg-white/10 px-4 py-2 shadow-lg backdrop-blur">
+          <div className="absolute bottom-2 left-[55%] right-2 flex justify-evenly rounded-md border bg-white/10 py-2 shadow-lg backdrop-blur">
             <CircularProgress
               value={32}
               color="danger"
-              size="sm"
+              size="md"
               showValueLabel
               classNames={{
                 value: "text-[7px] font-semibold",
@@ -95,7 +111,7 @@ const Hero = () => {
             <CircularProgress
               value={68}
               color="primary"
-              size="sm"
+              size="md"
               showValueLabel
               classNames={{
                 value: "text-[7px] font-semibold",
