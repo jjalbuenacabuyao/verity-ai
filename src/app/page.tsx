034f4aaf7423528f971617372358b6502603b3de
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-29 19:22:13 +0800

    add Features and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components";
+import Features from "@/components/Features";
 
 const Page = () => {
   return (
-    <>
+    <div className="grid gap-10">
       <Hero />
-    </>
+      <Features />
+    </div>
   );
 };
 
