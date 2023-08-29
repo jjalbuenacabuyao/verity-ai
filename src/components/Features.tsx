ffe44b402a64ab6a4610531d766ea7210e04b129
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-29 19:22:44 +0800

    increase icon size and add FeatureCard

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,39 +1,54 @@
 import React from "react";
 import { IoFlash } from "react-icons/io5";
-import { HiDocument } from "react-icons/hi";
+import { HiDocumentText } from "react-icons/hi";
 import { BsFillShieldLockFill } from "react-icons/bs";
-import { GiProcessor } from "react-icons/gi";
 import { MdCreditCardOff } from "react-icons/md";
+import { BsFillCpuFill } from "react-icons/bs"
+import FeatureCard from "./FeatureCard";
 
 const Features = () => {
   const features = [
     {
       title: "Fast",
       description: "",
-      icon: <IoFlash size={24} />,
+      icon: <IoFlash size={28} />,
     },
     {
       title: "Downloadable Detection Report",
       description: "",
-      icon: <HiDocument size={24} />,
+      icon: <HiDocumentText size={28} />,
     },
     {
       title: "Secure",
       description: "",
-      icon: <BsFillShieldLockFill size={24} />,
+      icon: <BsFillShieldLockFill size={28} />,
     },
     {
       title: "Cutting Edge LLM",
       description: "",
-      icon: <GiProcessor size={24} />,
+      icon: <BsFillCpuFill size={28} />,
     },
     {
       title: "Free",
       description: "",
-      icon: <MdCreditCardOff size={24} />,
+      icon: <MdCreditCardOff size={28} />,
     },
   ];
-  return <section id="features"></section>;
+  return (
+    <section id="features" className="mt-10 px-6">
+      <h2 className="text-center text-3xl font-semibold mb-8">Features</h2>
+      <div className="flex flex-col gap-6">
+        {features.map(({ title, description, icon }) => (
+          <FeatureCard
+            key={title}
+            title={title}
+            description={description}
+            icon={icon}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Features;
