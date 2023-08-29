d80806b5dc88d6974e9797f02fbc5ec332a4537b
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-29 23:56:47 +0800

    add descriptions

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,40 +3,46 @@ import { IoFlash } from "react-icons/io5";
 import { HiDocumentText } from "react-icons/hi";
 import { BsFillShieldLockFill } from "react-icons/bs";
 import { MdCreditCardOff } from "react-icons/md";
-import { BsFillCpuFill } from "react-icons/bs"
+import { BsFillCpuFill } from "react-icons/bs";
 import FeatureCard from "./FeatureCard";
 
 const Features = () => {
+  const iconSize = 28;
   const features = [
     {
       title: "Fast",
-      description: "",
-      icon: <IoFlash size={28} />,
+      description:
+        "VerityAI is designed to provide fast and efficient way of detecting AI-generated text.",
+      icon: <IoFlash size={iconSize} />,
     },
     {
       title: "Downloadable Detection Report",
-      description: "",
-      icon: <HiDocumentText size={28} />,
+      description:
+        "Detection result can be downloaded for more comprehensive view of the result and for future reference.",
+      icon: <HiDocumentText size={iconSize} />,
     },
     {
       title: "Secure",
-      description: "",
-      icon: <BsFillShieldLockFill size={28} />,
+      description:
+        "Only authorized educators can access the website, ensuring the security of your data.",
+      icon: <BsFillShieldLockFill size={iconSize} />,
     },
     {
       title: "Cutting Edge LLM",
-      description: "",
-      icon: <BsFillCpuFill size={28} />,
+      description:
+        "VerityAI uses RoBERTa Base Open AI detector, a modern Large Language Model(LLM), to analyze the texts and provide the detection results.",
+      icon: <BsFillCpuFill size={iconSize} />,
     },
     {
       title: "Free",
-      description: "",
-      icon: <MdCreditCardOff size={28} />,
+      description:
+        "Best part? VerityAI is completely free to use as it built using Open-source LLM so enjoy our services without any cost.",
+      icon: <MdCreditCardOff size={iconSize} />,
     },
   ];
   return (
     <section id="features" className="mt-10 px-6">
-      <h2 className="text-center text-3xl font-semibold mb-8">Features</h2>
+      <h2 className="mb-8 text-center text-3xl font-semibold">Features</h2>
       <div className="flex flex-col gap-6">
         {features.map(({ title, description, icon }) => (
           <FeatureCard
