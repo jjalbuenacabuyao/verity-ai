75befe9ce90bcb58de75a8182150c8cff6a08139
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-31 20:47:03 +0800

    animate the icon

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -18,7 +18,14 @@ const FeatureCard = ({ title, description, icon }: Props) => {
       className="flex flex-col gap-4 rounded-2xl border p-5"
     >
       <div className="flex items-center gap-3">
-        <div className="rounded-full bg-blue-100 p-3 text-blue-500">{icon}</div>
+        <motion.div
+          initial={{scale: 0.5}}
+          whileInView={{ scale: 1 }}
+          transition={{ type: "spring", duration: 0.5, delay: 0.3 }}
+          className="rounded-full bg-blue-100 p-3 text-blue-500"
+        >
+          {icon}
+        </motion.div>
         <h3 className="text-lg font-semibold">{title}</h3>
       </div>
       <p className="text-slate-500">{description}</p>
