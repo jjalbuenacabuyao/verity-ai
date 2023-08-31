a0713afe649d60bd0244ffa821c9fdbaf287e924
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-08-31 21:24:56 +0800

    add responsive padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     supports: {
@@ -12,61 +12,66 @@ module.exports = {
 
     extend: {
       colors: {
-        "header": "hsla(210, 20%, 98%, .8)",
+        header: "hsla(210, 20%, 98%, .8)",
         "off-black": "hsla(240, 6%, 25%, 0.122)",
         "light-blue": "#47caff",
         "light-violet": "#bd34fe",
       },
 
+      padding: {
+        responsive: "clamp(1.5rem, -0.8417rem + 9.9914vw, 8.75rem)",
+      },
+
       boxShadow: {
-        "dialog-content": "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+        "dialog-content":
+          "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
       },
 
       keyframes: {
-        "overlayShow": {
-          "from": { opacity: 0 },
-          "to": { opacity: 1 }
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
         },
-        "contentShow": {
-          "from": {
+        contentShow: {
+          from: {
             opacity: 0,
-            transform: "translate(-50%, -48%) scale(0.96)"
+            transform: "translate(-50%, -48%) scale(0.96)",
           },
-          "to": {
+          to: {
             opacity: 1,
-            transform: "translate(-50%, -50%) scale(1)"
-          }
-        },
-        "hide": {
-          "from": { opacity: 1 },
-          "to": { opacity: 0 }
-        },
-        "slideIn": {
-          "from": {
-            transform: "translateX(calc(100% + 24px))"
+            transform: "translate(-50%, -50%) scale(1)",
           },
-          "to": {
-            transform: "translateX(0)"
-          }
         },
-        "swipeOut": {
-          "from": {
-            transform: "translateX(var(--radix-toast-swipe-end-x))"
+        hide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        slideIn: {
+          from: {
+            transform: "translateX(calc(100% + 24px))",
           },
-          "to": {
-            transform: "translateX(calc(100% + 24px))"
-          }
-        }
+          to: {
+            transform: "translateX(0)",
+          },
+        },
+        swipeOut: {
+          from: {
+            transform: "translateX(var(--radix-toast-swipe-end-x))",
+          },
+          to: {
+            transform: "translateX(calc(100% + 24px))",
+          },
+        },
       },
 
       animation: {
-        "overlayShow": "overlayShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
-        "contentShow": "contentShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
-        "hide": 'hide 100ms ease-in',
-        "slideIn": 'slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        "swipeOut": 'swipeOut 100ms ease-out',
-      }
+        overlayShow: "overlayShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
+        hide: "hide 100ms ease-in",
+        slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        swipeOut: "swipeOut 100ms ease-out",
+      },
     },
   },
   plugins: [nextui()],
-}
+};
