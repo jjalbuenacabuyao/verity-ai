5891fe46702abd1b7481d3740b69959cfc60991f
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-01 21:43:22 +0800

    add responsive layout grid

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -15,11 +15,11 @@ const FeatureCard = ({ title, description, icon }: Props) => {
       initial={{ x: 100, opacity: 0 }}
       whileInView={{ x: 0, opacity: 1 }}
       transition={{ duration: 1, ease: "easeIn", type: "spring" }}
-      className="flex flex-col gap-4 rounded-2xl border p-5"
+      className="flex flex-col gap-4 rounded-2xl border p-5 md:last:col-span-2"
     >
       <div className="flex items-center gap-3">
         <motion.div
-          initial={{scale: 0.5}}
+          initial={{ scale: 0.5 }}
           whileInView={{ scale: 1 }}
           transition={{ type: "spring", duration: 0.5, delay: 0.3 }}
           className="rounded-full bg-blue-100 p-3 text-blue-500"
