8bf18b8cfe75ec162647d43cd115d81d05459f2d
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-03 11:22:10 +0800

    fix overflowing contents

--- src/components/home/FeatureCard.tsx
+++ src/components/home/FeatureCard.tsx
@@ -15,20 +15,20 @@ const FeatureCard = ({ title, description, icon }: Props) => {
       initial={{ x: 100, opacity: 0 }}
       whileInView={{ x: 0, opacity: 1 }}
       transition={{ duration: 1, ease: "easeIn", type: "spring" }}
-      className="flex flex-col gap-4 rounded-2xl border p-5 md:last:col-span-2"
+      className="group flex flex-col gap-4 rounded-2xl border p-5 md:last:col-span-2 hover:bg-blue-500 transition-colors ease-in duration-1000"
     >
       <div className="flex items-center gap-3">
         <motion.div
-          initial={{ scale: 0.5 }}
+          initial={{ scale: 0 }}
           whileInView={{ scale: 1 }}
           transition={{ type: "spring", duration: 0.5, delay: 0.3 }}
-          className="rounded-full bg-blue-100 p-3 text-blue-500"
+          className="rounded-full bg-blue-100 p-3 text-blue-500 group-hover:bg-white"
         >
           {icon}
         </motion.div>
-        <h3 className="text-lg font-semibold">{title}</h3>
+        <h3 className="text-lg font-semibold group-hover:text-white">{title}</h3>
       </div>
-      <p className="text-slate-500">{description}</p>
+      <p className="text-slate-500 group-hover:text-white">{description}</p>
     </motion.div>
   );
 };
