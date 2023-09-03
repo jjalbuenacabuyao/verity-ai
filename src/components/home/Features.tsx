052eb5ca8d04f977b0526cb68ad193df5b00460e
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-03 11:22:41 +0800

    add hover effect

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -41,7 +41,7 @@ const Features = () => {
     },
   ];
   return (
-    <section id="features" className="mt-10">
+    <section id="features" className="mt-10 relative overflow-hidden">
       <h2 className="mb-8 text-center text-3xl font-semibold">Features</h2>
       <div className="flex flex-col gap-6 md:grid md:grid-cols-2 lg:grid-cols-3">
         {features.map(({ title, description, icon }) => (
