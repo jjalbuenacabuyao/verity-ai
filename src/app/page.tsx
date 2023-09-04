731c643b2d6ceba95ea12e513a17cacef09220bb
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-04 19:00:14 +0800

    increase gap and add Tutorials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-"use client";
-
-import { Features, Hero } from "@/components";
+import { Features, Hero, Tutorials } from "@/components";
 
 const Page = () => {
   return (
-    <div className="grid gap-10 px-6 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
+    <div className="grid gap-12 px-6 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
       <Hero />
       <Features />
+      <Tutorials />
     </div>
   );
 };
