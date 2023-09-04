099e2b0602803ad6cab94481775f90ee49b0f47b
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-04 19:43:58 +0800

    remove grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Features, Hero, Tutorials } from "@/components";
 
 const Page = () => {
   return (
-    <div className="grid gap-12 px-6 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
+    <div className="px-6 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
       <Hero />
       <Features />
       <Tutorials />
