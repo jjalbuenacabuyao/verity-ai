4297f8ebb7a2097a32b326a647e71f07e1c2adef
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-05 18:53:10 +0800

    remove &apos in templated string

--- src/components/dashboard/EditRoleModal.tsx
+++ src/components/dashboard/EditRoleModal.tsx
@@ -40,7 +40,7 @@ const EditRoleModal = ({ isOpen, onOpenChange, onClose, firstName, role }: Props
                   </span>
                 </p>
                 <RadioGroup
-                  label={`Change ${firstName}&apos;s to:`}
+                  label={`Change ${firstName}'s to:`}
                   defaultValue={role === "ADMIN" ? "USER" : "ADMIN"}
                 >
                   {["Admin", "User"].map((item) => (
