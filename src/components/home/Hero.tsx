60bc69dd3e27c1b7d5b2275350da89bd250da6cf
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-08 19:59:49 +0800

    add functionality to the get started button

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { workSans, inter } from "@/fonts";
 import { Button } from "@nextui-org/button";
 import HeroIllustration from "./HeroIllustration";
+import { useDisclosure } from "@nextui-org/modal";
+import { LogInModal } from "../utilities";
+import { useCurrentUserContext } from "@/hooks/userContext";
+import { useRouter } from "next/navigation";
 
 const Hero = () => {
+  const currentUser = useCurrentUserContext();
+  const router = useRouter();
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
   return (
     <div className="relative flex flex-col items-center gap-32 lg:grid lg:h-[calc(100%-64px)] lg:max-h-[560px] lg:grid-cols-2 lg:items-center lg:gap-0 lg:supports-vh:h-[calc(100vh-64px)] lg:supports-dvh:h-[calc(100dvh-64px)]">
       <div className="grid gap-4 pt-12 sm:pt-16 md:justify-items-start md:pt-20 lg:pr-5 lg:pt-0">
@@ -23,9 +33,15 @@ const Hero = () => {
           radius="sm"
           size="lg"
           className="mt-4 font-semibold hover:bg-blue-800 hover:shadow-lg"
+          onPress={currentUser ? () => router.push("/detector") : onOpen}
         >
           Get Started
         </Button>
+        <LogInModal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          onClose={onClose}
+        />
       </div>
 
       <HeroIllustration />
