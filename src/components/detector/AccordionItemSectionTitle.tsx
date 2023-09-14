26a1f3f7312078ef5bea4a92404ecee012dcfae7
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-14 19:59:52 +0800

    add testid

--- src/components/detector/AccordionItemSectionTitle.tsx
+++ src/components/detector/AccordionItemSectionTitle.tsx
@@ -4,12 +4,12 @@ import { DiTerminalBadge } from "react-icons/di";
 interface Props {
   title: string;
   description: string;
-};
+}
 
 const AccordionItemSectionTitle = ({ title, description }: Props) => {
   return (
     <div className="mb-3 grid grid-cols-[auto_1fr] gap-2">
-      <DiTerminalBadge size={24} />
+      <DiTerminalBadge size={24} data-testid="terminalIcon" />
       <div>
         <p className="font-medium">{title}</p>
         <p className="text-sm">{description}</p>
