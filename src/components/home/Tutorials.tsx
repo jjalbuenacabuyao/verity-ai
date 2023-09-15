b8b96a10006e4f48b7091a4e82a1c751a98c67b9
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-15 20:54:15 +0800

    add 1 and 2 text tutorial

--- src/components/home/Tutorials.tsx
+++ src/components/home/Tutorials.tsx
@@ -5,10 +5,9 @@ import { MdVideoLibrary } from "react-icons/md";
 import { BsFillFileRichtextFill } from "react-icons/bs";
 import React, { useState } from "react";
 import Subheading from "./Subheading";
+import Image from "next/image";
 
-type Props = {};
-
-const Tutorials = (props: Props) => {
+const Tutorials = () => {
   const [selected, setSelected] = useState("Video");
   return (
     <div id="tutorial" className="pt-[4.5rem]">
@@ -17,10 +16,12 @@ const Tutorials = (props: Props) => {
         <Tabs
           aria-label="Tutorial options"
           color="primary"
-          variant="underlined"
+          variant="solid"
+          radius="full"
           classNames={{
             base: "flex justify-center",
-            tab: "h-12",
+            tab: "h-12 px-5",
+            tabList: "gap-5",
           }}
           selectedKey={selected}
           //@ts-ignore
@@ -53,7 +54,47 @@ const Tutorials = (props: Props) => {
               </div>
             }
           >
-            <div></div>
+            <div className="mx-auto max-w-2xl lg:text-lg">
+              <h3 className="mb-8 mt-4 text-center text-3xl font-semibold">
+                VerityAI Step-by-Step Instructions
+              </h3>
+              <ol className="flex flex-col gap-8">
+                <li className="grid gap-4">
+                  <p>
+                    1. In the VerityAI homepage, click the{" "}
+                    <span className="font-semibold">Log in</span> button.
+                  </p>
+                  <Image
+                    src={"/login.png"}
+                    alt="Screenshot of VerityAI homepage with arrow pointing to the Login button"
+                    width={100}
+                    height={100}
+                    className="mx-auto h-auto w-full max-w-lg rounded-lg border shadow-md"
+                    quality={100}
+                    unoptimized
+                    priority
+                  />
+                </li>
+
+                <li className="grid gap-4">
+                  <p>
+                    2. Enter your <span className="font-semibold">email</span> and{" "}
+                    <span className="font-semibold">password</span> and click
+                    Log in.
+                  </p>
+                  <Image
+                    src={"/emailandpassword.png"}
+                    alt="Screenshot of VerityAI homepage with arrow pointing to the Login button"
+                    width={100}
+                    height={100}
+                    className="mx-auto h-auto w-full max-w-lg rounded-lg border shadow-md"
+                    quality={100}
+                    unoptimized
+                    priority
+                  />
+                </li>
+              </ol>
+            </div>
           </Tab>
         </Tabs>
       </div>
