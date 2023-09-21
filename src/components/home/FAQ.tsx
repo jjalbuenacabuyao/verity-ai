6e68d053c3e06deef452685c7a522d4cb87e80ea
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-21 20:48:47 +0800

    use FaqAccordion

--- src/components/home/FAQ.tsx
+++ src/components/home/FAQ.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Subheading from './Subheading';
+import React from "react";
+import Subheading from "./Subheading";
+import FaqAccordion from "./FaqAccordion";
 
 const FAQ = () => {
   return (
-    <div id='faqs' className="pt-[4.5rem]">
+    <div id="faqs" className="pt-[4.5rem]">
       <Subheading title="Frequently Asked Questions" />
+      <FaqAccordion />
     </div>
   );
-}
+};
 
-export default FAQ
+export default FAQ;
