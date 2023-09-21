29fba80493fac6870197d8cf12a205bb1f087092
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-09-21 20:25:03 +0800

    initial commit

---
+++ src/components/home/FAQ.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Subheading from './Subheading';
+
+const FAQ = () => {
+  return (
+    <div id='faqs' className="pt-[4.5rem]">
+      <Subheading title="Frequently Asked Questions" />
+    </div>
+  );
+}
+
+export default FAQ
