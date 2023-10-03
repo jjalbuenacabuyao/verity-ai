32d2b892f7f32e51c73d8e6c355b6434b86a99b1
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:17:52 +0800

    changerole documentation

--- src/app/api/changerole/route.ts
+++ src/app/api/changerole/route.ts
@@ -4,26 +4,33 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+/**
+ * Handles a POST request to update the role of a user in a database using the Prisma ORM.
+ *
+ * @param request - The HTTP request object containing the user's data.
+ * @returns {Promise<NextResponse>} A JSON response object indicating the success of the operation.
+ */
+
 export async function POST(request: Request) {
   const body = await request.json();
-  const { id, role} = body;
+  const { id, role } = body;
 
   const user = await prisma.user.update({
     where: {
       id,
     },
     data: {
-      role
-    }
+      role,
+    },
   });
 
   if (!user) {
     return NextResponse.json({
-      success: false
-    })
+      success: false,
+    });
   }
 
   return NextResponse.json({
-    success: true
-  })
+    success: true,
+  });
 }
