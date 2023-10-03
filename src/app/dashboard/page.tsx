d52bba1f7e621cd51694bf46c41feb0fc89d463b
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:19:16 +0800

    dashboard documentation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,14 @@ import getCurrentUser from "../actions/getCurrentUser";
 import client from "../libs/prismadb";
 import { DashboardContents } from "@/components/dashboard";
 
+/**
+ * Renders the dashboard page.
+ * 
+ * Retrieves the current user and the total number of users from the database. If the current user is not an admin, it renders the `AccessDenied` component. Otherwise, it renders the `DashboardContents` component with the total number of users passed as a prop.
+ * 
+ * @returns {Promise<JSX.Element>} JSX elements representing the dashboard page.
+ */
+
 const Dashboard = async () => {
   const currentUser = await getCurrentUser();
   const totalUsers = (await client.user.findMany()).length;
