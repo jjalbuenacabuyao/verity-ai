384e12663382b7f5d4546aeed7ad95a523835f3d
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 20:24:17 +0800

    add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { FAQ, Features, Hero, Tutorials } from "@/components/home";
+import { FAQ, Features, Footer, Hero, Tutorials } from "@/components/home";
 
 /**
  * Renders the homepage.
@@ -13,6 +13,7 @@ const Page = () => {
       <Features />
       <Tutorials />
       <FAQ />
+      <Footer />
     </div>
   );
 };
