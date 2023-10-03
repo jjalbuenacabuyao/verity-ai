69d26d86af95a469ca1bf173921f2acde0f705d2
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:21:11 +0800

    getTextFromFiles documentation

--- src/utils/getTextFromFiles.ts
+++ src/utils/getTextFromFiles.ts
@@ -1,5 +1,12 @@
 import mammoth from "mammoth";
 
+/**
+ * Extracts text from either a PDF or a DOCX file.
+ * 
+ * @param file - The file from which to extract text. It can be either a PDF or a DOCX file.
+ * @returns {string} The extracted text from the file.
+ */
+
 export default async function getTextFromFiles(file: File) {
   const pdfFileType = "application/pdf";
   const docxFileType =
