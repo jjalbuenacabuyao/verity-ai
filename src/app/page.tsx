542973336d51b0183bbbe1c90651a56bc8bb6d67
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:13:42 +0800

    homepage documentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { FAQ, Features, Hero, Tutorials } from "@/components/home";
 
+/**
+ * Renders the homepage.
+ *
+ * @returns {JSX.Element} The rendered component.
+ */
+
 const Page = () => {
   return (
     <div className="px-6 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
