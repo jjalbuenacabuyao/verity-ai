4fc3d20926b28419cd4db1518bec96f149b2c793
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:18:14 +0800

    deleteuser documentation

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -3,15 +3,22 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+/**
+ * Handles a POST request to delete a user from the database.
+ *
+ * @param request - The request object containing the JSON payload with the user ID.
+ * @returns {Promise<NextResponse>} A JSON response indicating success.
+ */
+
 export async function POST(request: Request) {
-  const body = await request.json()
+  const body = await request.json();
   const { id } = body;
 
   const deletedUser = await prisma.user.delete({
     where: {
       id: id,
-    }
-  })
+    },
+  });
 
-  return NextResponse.json("Success")
+  return NextResponse.json("Success");
 }
