6c281e6092796cfb1b9dfdc0801d8ee1d589ddda
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-10-03 19:20:50 +0800

    getSentencePercentage documentation

--- src/utils/getSentencePercentage.ts
+++ src/utils/getSentencePercentage.ts
@@ -1,3 +1,11 @@
+/**
+ * Calculates the percentage of sentence characters in relation to the total number of characters in a given text.
+ *
+ * @param {string} text - The input text for which the sentence percentage needs to be calculated.
+ * @param {number} totalCharacters - The total number of characters in the text.
+ * @returns {number} The percentage of sentence characters in relation to the total number of characters in the text.
+ */
+
 export default function getSentencePercentage(
   text: string,
   totalCharacters: number
