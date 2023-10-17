0c34aa52c2aa1833ec6505a43d34c83280838d43
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-10-17 18:22:22 +0800

    edit the comment

--- src/app/detector/page.tsx
+++ src/app/detector/page.tsx
@@ -6,9 +6,11 @@ import { AccessDenied } from "@/components/utilities";
 import { Aside, ResultContainer } from "@/components/detector";
 
 /**
- * Renders a file detection feature.
+ * Renders the detector page.
  * 
- * React component that checks if the user is logged in and displays an access denied message if not. It consists of an `Aside` component for file upload and a `ResultContainer` component for displaying the detection results.
+ * React component that checks if the user is logged in and renders the AccessDenied component if not. 
+ * It consists of an `Aside` component for file upload and a `ResultContainer` component for displaying
+ * the detection results.
  * 
  * @returns {React.JSX.Element} The rendered component based on the user's authentication status.
  */
@@ -22,7 +24,9 @@ const Detector = () => {
   }
 
   return (
-    <div className="px-6 pb-10 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:grid lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
+    <div className="px-6 pb-10 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:grid 
+    lg:grid-cols-[1fr_2.5fr] lg:gap-10 lg:px-24 lg:supports-clamp:px-fluid xl:px-28
+    xl:supports-clamp:px-fluid">
       <Aside setFiles={setFiles} />
       <ResultContainer files={files} />
     </div>
