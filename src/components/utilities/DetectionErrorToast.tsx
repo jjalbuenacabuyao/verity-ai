76772d4de6d99a7e08f6d171ccf704fa3dd152dd
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-11-12 22:05:05 +0800

    remove error message

--- src/components/utilities/DetectionErrorToast.tsx
+++ src/components/utilities/DetectionErrorToast.tsx
@@ -9,18 +9,17 @@ import {
   Viewport,
 } from "@radix-ui/react-toast";
 import { BiErrorCircle } from "react-icons/bi";
-import { AiFillCloseCircle } from "react-icons/ai"
+import { AiFillCloseCircle } from "react-icons/ai";
 
 type Props = {
   filename: string;
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-  errorMessage: string;
 };
 
 /**
  * A component that renders a toast notification for displaying an error message.
- * 
+ *
  * @param {Object} props - The component props.
  * @param {string} props.filename - The name of the file that caused the error.
  * @param {boolean} props.isOpen - A state variable indicating whether the toast notification is open or closed.
@@ -28,17 +27,23 @@ type Props = {
  * @returns {JSX.Element} - The rendered component.
  */
 
-const DetectionErrorToast: React.FC<Props> = ({ isOpen, setIsOpen, filename, errorMessage }: Props) => {
+const DetectionErrorToast: React.FC<Props> = ({
+  isOpen,
+  setIsOpen,
+  filename,
+}: Props) => {
   return (
     <>
       <Root
         open={isOpen}
         onOpenChange={setIsOpen}
-        className="grid grid-cols-[max-content_auto_max-content] grid-rows-2 grid-area-toast items-center rounded-md bg-red-500 p-4 text-white shadow-md gap-x-6 gap-y-2 data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut">
-        
+        className="grid-area-toast grid grid-cols-[max-content_auto_max-content] grid-rows-2 items-center gap-x-6 gap-y-2 rounded-md bg-red-500 p-4 text-white shadow-md data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[state=closed]:animate-hide data-[state=open]:animate-slideIn data-[swipe=end]:animate-swipeOut data-[swipe=cancel]:transition-[transform_200ms_ease-out]"
+      >
         <BiErrorCircle size={32} className="area-error-icon" />
 
-        <Title className="area-title leading-none text-sm">{errorMessage}</Title>
+        <Title className="area-title text-sm leading-none">
+          Error occured while processing:
+        </Title>
 
         <Description className="area-description font-medium leading-none">
           {filename}
