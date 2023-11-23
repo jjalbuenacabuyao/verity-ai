fa6820da6c9b3360b7cd37550c6bff7405190a62
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-11-23 20:53:47 +0800

    remove deleted files

--- src/components/utilities/index.ts
+++ src/components/utilities/index.ts
@@ -1,6 +1,4 @@
 import AccessDenied from "./AccessDenied";
-import DetectionErrorToast from "./DetectionErrorToast";
-import EmailAlreadyExistToast from "./EmailAlreadyExistToast";
 import EyeFilledIcon from "./EyeFilledIcon";
 import EyeSlashFilledIcon from "./EyeSlashFilledIcon";
 import Header from "./Header";
@@ -12,13 +10,10 @@ import MailIcon from "./MailIcon";
 import PasswordVisibilityToggler from "./PasswordVisibilityToggler";
 import PlusIcon from "./PlusIcon";
 import Toast from "./Toast";
-import UserAddedToast from "./UserAddedToast";
 import UserMenu from "./UserMenu";
 
 export {
   AccessDenied,
-  DetectionErrorToast,
-  EmailAlreadyExistToast,
   EyeFilledIcon,
   EyeSlashFilledIcon,
   Header,
@@ -30,6 +25,5 @@ export {
   PasswordVisibilityToggler,
   PlusIcon,
   Toast,
-  UserAddedToast,
   UserMenu,
 };
