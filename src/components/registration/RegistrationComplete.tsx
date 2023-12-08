832ab2cee57ee76b083c647856b9c78426a4bb5f
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-08 17:39:12 +0800

    message for completing registration

---
+++ src/components/registration/RegistrationComplete.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const RegistrationComplete = (props: Props) => {
+  return (
+    <div>RegistrationComplete</div>
+  )
+}
+
+export default RegistrationComplete
