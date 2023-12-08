16ae498e926c57b70a59ae1a866870d924a93506
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-08 17:38:34 +0800

    import and export components

---
+++ src/components/registration/index.ts
@@ -0,0 +1,15 @@
+import DataPrivacyConsent from "./DataPrivacyConsent";
+import EmailAndPasswordInput from "./EmailAndPasswordInput";
+import NameInput from "./NameInput";
+import Registration from "./Registration";
+import RegistrationComplete from "./RegistrationComplete";
+import TermsAndCondition from "./TermsAndCondition";
+
+export {
+  DataPrivacyConsent,
+  EmailAndPasswordInput,
+  NameInput,
+  Registration,
+  RegistrationComplete,
+  TermsAndCondition,
+}
