985a76b4d29ed172fc4a73c312e5560ab8c18035
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-08 17:38:10 +0800

    data privacy consent

---
+++ src/components/registration/DataPrivacyConsent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const DataPrivacyConsent = (props: Props) => {
+  return (
+    <div>DataPrivacyConsent</div>
+  )
+}
+
+export default DataPrivacyConsent
