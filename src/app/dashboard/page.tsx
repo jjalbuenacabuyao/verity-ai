fa127296311b74a1d91617c7d41ca1ee2894ce76
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-08 17:36:24 +0800

    change the title

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,17 @@ import { AccessDenied } from "@/components/utilities";
 import getCurrentUser from "../actions/getCurrentUser";
 import client from "../libs/prismadb";
 import { DashboardContents } from "@/components/dashboard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 /**
  * Renders the dashboard page.
- * 
+ *
  * Retrieves the current user and the total number of users from the database. If the current user is not an admin, it renders the `AccessDenied` component. Otherwise, it renders the `DashboardContents` component with the total number of users passed as a prop.
- * 
+ *
  * @returns {Promise<JSX.Element>} JSX elements representing the dashboard page.
  */
 
@@ -31,7 +36,10 @@ const Dashboard = async () => {
 
   return (
     <div className="px-6 pb-10 pt-8 supports-clamp:px-fluid md:px-16 md:supports-clamp:px-fluid lg:px-24 lg:supports-clamp:px-fluid xl:px-28 xl:supports-clamp:px-fluid">
-      <DashboardContents totalUsers={totalUsers} currentUserEmail={currentUser.email} />
+      <DashboardContents
+        totalUsers={totalUsers}
+        currentUserEmail={currentUser.email}
+      />
     </div>
   );
 };
