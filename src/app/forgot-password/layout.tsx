08a1d20a63d57beea0faac1edf1bc5ef7bba1117
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-08 17:37:20 +0800

    add layout, remove header

---
+++ src/app/forgot-password/layout.tsx
@@ -0,0 +1,17 @@
+import { inter } from "@/fonts";
+
+export default function ForgotPasswordLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${inter.className} mx-auto max-w-screen-2xl bg-white/95 text-slate-700 antialiased`}
+      >
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
