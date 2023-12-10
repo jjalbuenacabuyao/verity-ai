0876d78611d81b1bb8464b12a9fbdc4c08ed23b1
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-10 21:45:19 +0800

    initial commit

---
+++ src/app/api/sendregistrationlink/route.ts
@@ -0,0 +1,40 @@
+import nodemailer from "nodemailer";
+import crypto from "crypto";
+import client from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  const token = crypto.randomBytes(20).toString("hex");
+  const expiration = new Date(Date.now() + 3600000);
+
+  await client.registrationTokens.create({
+    data: {
+      registrationToken: token,
+      expiration: expiration,
+    },
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    to: email,
+    from: process.env.EMAIL_FROM,
+    subject: "Registration link",
+    text: `You are receiving this because you have requested a VerityAI account.\n\nPlease click on the following link, or paste this into your browser to complete the registration process within one hour of receiving it:\n\nhttp://localhost:3000/registration/${token}\n\nIf you did not request this, please ignore this email.\n`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return NextResponse.json({ status: 200 });
+  } catch (e) {
+    return NextResponse.json({ status: 500 });
+  }
+}
