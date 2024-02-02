31ef37a85692e54b51fd9467501025c058656f0c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-02-02 16:47:52 +0800

    remove leading

--- src/components/registration/RegistrationComplete.tsx
+++ src/components/registration/RegistrationComplete.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const RegistrationComplete = (props: Props) => {
   return (
     <div className="flex flex-col gap-5 items-center text-center">
-      <h1 className="text-4xl font-bold text-blue-500 leading-4">Registration Complete</h1>
+      <h1 className="text-4xl font-bold text-blue-500">Registration Complete</h1>
       <LogInButton />
     </div>
   );
